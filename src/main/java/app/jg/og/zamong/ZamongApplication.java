1e4c06123a1db6d453562c89d26ce3e236dd3ec6
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-08-09 10:11:59 +0900

    first commit

---
+++ src/main/java/app/jg/og/zamong/ZamongApplication.java
@@ -0,0 +1,13 @@
+package app.jg.og.zamong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZamongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZamongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/jg/og/zamong/ZamongApplicationTests.java
@@ -0,0 +1,13 @@
+package app.jg.og.zamong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZamongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
