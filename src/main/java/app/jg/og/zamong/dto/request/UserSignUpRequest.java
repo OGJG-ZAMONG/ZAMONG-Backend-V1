2a80635ff289760fc93de5caf7a5b5539b900811
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-08-23 20:27:43 +0900

    :bookmark_tabs: :: (#9) <UserSignUpRequest> signup

---
+++ src/main/java/app/jg/og/zamong/dto/request/UserSignUpRequest.java
@@ -0,0 +1,15 @@
+package app.jg.og.zamong.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserSignUpRequest {
+
+    private String name;
+
+    private String email;
+
+    private String id;
+
+    private String password;
+}
