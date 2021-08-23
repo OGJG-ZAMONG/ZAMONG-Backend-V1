eb8f3f67f3d58159c3f609abd5fcd48374ad8e8d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-08-23 20:28:09 +0900

    :bookmark_tabs: :: (#9) <AuthController> auth controller

---
+++ src/main/java/app/jg/og/zamong/controller/AuthController.java
@@ -0,0 +1,13 @@
+package app.jg.og.zamong.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+
+}
