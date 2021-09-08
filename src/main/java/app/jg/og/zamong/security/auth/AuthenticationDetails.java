bab2517e86a71d91a2efa769a752cdecdc587233
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-08 23:29:30 +0900

    :bookmark_tabs: :: (#9) <AuthenticationDetails>

---
+++ src/main/java/app/jg/og/zamong/security/auth/AuthenticationDetails.java
@@ -0,0 +1,49 @@
+package app.jg.og.zamong.security.auth;
+
+import app.jg.og.zamong.entity.user.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+public class AuthenticationDetails implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
