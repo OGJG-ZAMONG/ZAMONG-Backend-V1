32fee40445542c5a7d3fde7011bc2bbfac97b4e3
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-08 23:36:08 +0900

    ⚡️ :: (#9) <UserRepository> jpa method

--- src/main/java/app/jg/og/zamong/entity/user/UserRepository.java
+++ src/main/java/app/jg/og/zamong/entity/user/UserRepository.java
@@ -2,8 +2,11 @@ package app.jg.og.zamong.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
+    Optional<User> findByUuid(String uuid);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailOrId(String email, String id);
 }
