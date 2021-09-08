15040c17f89b6acef7d3d09b0b867f8ba2e06320
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-08 23:35:20 +0900

    ⚡️ :: (#9) <AuthService> login, signup

--- src/main/java/app/jg/og/zamong/service/auth/AuthService.java
+++ src/main/java/app/jg/og/zamong/service/auth/AuthService.java
@@ -1,8 +1,11 @@
 package app.jg.og.zamong.service.auth;
 
-import app.jg.og.zamong.dto.request.UserSignUpRequest;
+import app.jg.og.zamong.dto.request.LoginUserRequest;
+import app.jg.og.zamong.dto.request.SignUpUserRequest;
+import app.jg.og.zamong.dto.response.IssueTokenResponse;
 import app.jg.og.zamong.entity.user.User;
 
 public interface AuthService {
-    User createUser(UserSignUpRequest request);
+    User registerUser(SignUpUserRequest request);
+    IssueTokenResponse loginUser(LoginUserRequest request);
 }
--- src/main/java/app/jg/og/zamong/service/auth/AuthServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/auth/AuthServiceImpl.java
@@ -1,24 +1,55 @@
 package app.jg.og.zamong.service.auth;
 
-import app.jg.og.zamong.dto.request.UserSignUpRequest;
+import app.jg.og.zamong.dto.request.LoginUserRequest;
+import app.jg.og.zamong.dto.request.SignUpUserRequest;
+import app.jg.og.zamong.dto.response.IssueTokenResponse;
 import app.jg.og.zamong.entity.user.User;
 import app.jg.og.zamong.entity.user.UserRepository;
+import app.jg.og.zamong.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class AuthServiceImpl implements AuthService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
     private final UserRepository userRepository;
 
     @Override
-    public User createUser(UserSignUpRequest request) {
+    public User registerUser(SignUpUserRequest request) {
+        userRepository.findByEmail(request.getEmail())
+                .ifPresent((user) -> {
+                    throw new RuntimeException();
+                });
+
         return this.userRepository.save(User.builder()
                 .name(request.getName())
                 .email(request.getEmail())
                 .id(request.getId())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .build());
+    }
+
+    @Override
+    public IssueTokenResponse loginUser(LoginUserRequest request) {
+        User user = verifyUser(request);
+        return generateIssueTokenResponse(user);
+    }
+
+    private User verifyUser(LoginUserRequest request) throws RuntimeException {
+        return userRepository.findByEmailOrId(request.getUserIdentity(), request.getUserIdentity())
+                .filter(user -> passwordEncoder.matches(request.getPassword(), user.getPassword()))
+                .orElseThrow(RuntimeException::new);
+    }
+
+    private IssueTokenResponse generateIssueTokenResponse(User user) {
+        return IssueTokenResponse.builder()
+                .accessToken(jwtTokenProvider.generateAccessToken(user.getUuid()))
+                .refreshToken(jwtTokenProvider.generateRefreshToken(user.getUuid()))
+                .build();
     }
 }
