2a7f55ae326f258d1d611a31165baf6f66d426c8
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-08 23:34:53 +0900

    ⚡️ :: (#9) <AuthController> signup

--- src/main/java/app/jg/og/zamong/controller/AuthController.java
+++ src/main/java/app/jg/og/zamong/controller/AuthController.java
@@ -1,6 +1,8 @@
 package app.jg.og.zamong.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-
+    @PostMapping("/signup")
+    public String register() {
+        return "o";
+    }
 }
