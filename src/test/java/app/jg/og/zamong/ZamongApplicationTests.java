f692a554f82f530e9a6bde36613e07857faf9676
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-08 23:43:25 +0900

    :bug: :: (#9) <ZamongApplicationTests> contextload 메서드 삭제

--- src/test/java/app/jg/og/zamong/ZamongApplicationTests.java
+++ src/test/java/app/jg/og/zamong/ZamongApplicationTests.java
@@ -1,13 +1,7 @@
 package app.jg.og.zamong;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ZamongApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
