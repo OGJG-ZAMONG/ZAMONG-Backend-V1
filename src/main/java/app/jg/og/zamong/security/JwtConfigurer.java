0631d34b3c8d6363484cab0dbc20343d94d6bcd7
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-08 23:30:46 +0900

    :bookmark_tabs: :: (#9) <JwtConfigurer>

---
+++ src/main/java/app/jg/og/zamong/security/JwtConfigurer.java
@@ -0,0 +1,19 @@
+package app.jg.og.zamong.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        httpSecurity.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
