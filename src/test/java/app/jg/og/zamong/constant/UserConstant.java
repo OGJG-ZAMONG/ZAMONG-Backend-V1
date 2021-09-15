4d93494ea92ef6cf580fa1ed97367cb07bff6698
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-15 20:29:14 +0900

    :bookmark_tabs: :: (#12) <SecurityConstant> 테스트용 상수 정의

---
+++ src/test/java/app/jg/og/zamong/constant/UserConstant.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.constant;
+
+public class UserConstant {
+    public static final String NAME = "name";
+    public static final String EMAIL = "zamong@gmail.com";
+    public static final String ID = "id";
+    public static final String PASSWORD = "password";
+}
