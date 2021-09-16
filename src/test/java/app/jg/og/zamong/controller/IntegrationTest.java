03c378d2cde00842a8597135a687396373a49f91
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-16 23:39:12 +0900

    :bookmark_tabs: :: (#12) <IntegrationTest> 통합 테스트용 부모 클래스

---
+++ src/test/java/app/jg/og/zamong/controller/IntegrationTest.java
@@ -0,0 +1,18 @@
+package app.jg.og.zamong.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+@Disabled
+public class IntegrationTest {
+    @Autowired protected MockMvc mockMvc;
+}
