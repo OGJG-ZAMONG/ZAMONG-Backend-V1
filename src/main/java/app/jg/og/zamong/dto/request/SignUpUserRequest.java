f0c9528d812533251c0a2933aee1c1f20a26baf4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-19 23:43:30 +0900

    ⚡️ :: (#12) <SignUpUserRequest> 유효성 검증

--- src/main/java/app/jg/og/zamong/dto/request/SignUpUserRequest.java
+++ src/main/java/app/jg/og/zamong/dto/request/SignUpUserRequest.java
@@ -4,18 +4,30 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.*;
+
 @Builder
 @Getter
 public class SignUpUserRequest {
 
+    @NotBlank
+    @Size(min = 1, max = 6)
     private final String name;
 
+    @NotBlank
+    @Email
     private final String email;
 
-    @JsonProperty("authentication_code")
+    @JsonProperty("d")
+    @Size(min = 6, max = 6)
     private final String authenticationCode;
 
+    @NotBlank
+    @Size(min = 6, max = 16)
     private final String id;
 
+    @NotBlank
+    @Size(min = 8, max = 24)
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$")
     private final String password;
 }
