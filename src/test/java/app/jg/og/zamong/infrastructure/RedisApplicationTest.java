9d81df0f5cde912384982592ee3820085a0d1935
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-22 01:52:36 +0900

    :test_tube: :: (#12) <RedisApplicationTest> 레포지토리 테스트

--- src/test/java/app/jg/og/zamong/infrastructure/RedisApplicationTest.java
+++ src/test/java/app/jg/og/zamong/infrastructure/RedisApplicationTest.java
@@ -1,6 +1,8 @@
 package app.jg.og.zamong.infrastructure;
 
 import app.jg.og.zamong.controller.IntegrationTest;
+import app.jg.og.zamong.entity.redis.authenticationcode.AuthenticationCode;
+import app.jg.og.zamong.entity.redis.authenticationcode.AuthenticationCodeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -23,5 +25,23 @@ public class RedisApplicationTest extends IntegrationTest {
 
         String result = valueOperations.get(key);
         assertThat(data).isEqualTo(result);
+    }
+
+    @Autowired
+    private AuthenticationCodeRepository authenticationCodeRepository;
+
+    @Test
+    void redisRepositoryTest() {
+        String emailKey = "jiwoourty@gmail.com";
+        String codeData = "123456";
+
+        AuthenticationCode code = new AuthenticationCode(emailKey, codeData);
+
+        authenticationCodeRepository.save(code);
+
+        AuthenticationCode result = authenticationCodeRepository.findById(emailKey)
+                .orElse(new AuthenticationCode("", ""));
+
+        assertThat(result.getCode()).isEqualTo(codeData);
     }
 }
