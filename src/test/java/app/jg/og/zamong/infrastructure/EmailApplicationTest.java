1b1de4a9de4515e7d11ed4d45db178160b689ccb
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-23 00:18:58 +0900

    :test_tube: :: (#21) <EmailApplicationTest> 인증코드 생성 로직 수정

--- src/test/java/app/jg/og/zamong/infrastructure/EmailApplicationTest.java
+++ src/test/java/app/jg/og/zamong/infrastructure/EmailApplicationTest.java
@@ -4,19 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EmailApplicationTest {
 
-    public static final Random RANDOM = new Random();
+    public static final Random RANDOM = new Random(System.currentTimeMillis());
 
     @Test
     void 인증코드는_랜덤() {
-        RANDOM.setSeed(System.currentTimeMillis());
-
         String code1 = createAuthenticationCode();
         String code2 = createAuthenticationCode();
 
@@ -26,12 +23,7 @@ public class EmailApplicationTest {
     }
 
     private String createAuthenticationCode() {
-        StringBuilder sb = new StringBuilder();
-        // 한자리씩 replace 해야 됨
-        for(int i=0; i<6; i++) {
-            sb.append(RANDOM.nextInt(10) % 10);
-        }
-        return sb.toString();
+       return String.format("%06d", RANDOM.nextInt(1000000) % 1000000);
     }
 
     @Test
