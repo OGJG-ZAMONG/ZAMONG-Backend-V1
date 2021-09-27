60c70ba272d219678dcef61cf1192b37e4ef30cf
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-27 18:27:15 +0900

    ⚡️ :: (#27) <AuthService> MailSentResponse 반환값 설정

--- src/main/java/app/jg/og/zamong/service/auth/AuthService.java
+++ src/main/java/app/jg/og/zamong/service/auth/AuthService.java
@@ -4,10 +4,11 @@ import app.jg.og.zamong.dto.request.EmailAuthenticationRequest;
 import app.jg.og.zamong.dto.request.LoginUserRequest;
 import app.jg.og.zamong.dto.request.SignUpUserRequest;
 import app.jg.og.zamong.dto.response.IssueTokenResponse;
+import app.jg.og.zamong.dto.response.MailSentResponse;
 import app.jg.og.zamong.dto.response.SignedUserResponse;
 
 public interface AuthService {
     SignedUserResponse registerUser(SignUpUserRequest request);
     IssueTokenResponse loginUser(LoginUserRequest request);
-    void sendOutAuthenticationEmail(EmailAuthenticationRequest request);
+    MailSentResponse sendOutAuthenticationEmail(EmailAuthenticationRequest request);
 }
