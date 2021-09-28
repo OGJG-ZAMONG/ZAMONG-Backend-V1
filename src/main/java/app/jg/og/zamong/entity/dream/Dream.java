be7e2b0348a4af67306b5d56b848c45cafe2eb23
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-29 08:15:49 +0900

    :bookmark_tabs: :: (#31) <Dream> dream entity

---
+++ src/main/java/app/jg/og/zamong/entity/dream/Dream.java
@@ -0,0 +1,32 @@
+package app.jg.og.zamong.entity.dream;
+
+import app.jg.og.zamong.entity.user.User;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Dream {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID uuid;
+
+    @Column(length = 100)
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String content;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_uuid", nullable = false)
+    private User user;
+}
