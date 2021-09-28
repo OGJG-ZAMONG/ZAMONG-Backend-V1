6d0ae651934a2d01523da2211838e2252717350d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-28 20:12:05 +0900

    ⚡️ :: (#29) <UserRepository> findById 메서드

--- src/main/java/app/jg/og/zamong/entity/user/UserRepository.java
+++ src/main/java/app/jg/og/zamong/entity/user/UserRepository.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUuid(UUID uuid);
+    Optional<User> findById(String id);
     Optional<User> findByEmail(String email);
     @Query("select u from User u where u.email = :id or u.id = :id")
     Optional<User> findByEmailOrId(@Param("id") String userId);
