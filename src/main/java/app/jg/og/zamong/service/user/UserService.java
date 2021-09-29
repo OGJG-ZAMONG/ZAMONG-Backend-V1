86ac50f828583455f6e58634da50cbcb30eef77c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-29 13:26:56 +0900

    :bookmark_tabs: :: (#31) <UserService> 인터페이스 정의

---
+++ src/main/java/app/jg/og/zamong/service/user/UserService.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.service.user;
+
+import app.jg.og.zamong.dto.response.UserInformationResponse;
+
+public interface UserService {
+
+    UserInformationResponse queryUserInformation(String uuid);
+}
