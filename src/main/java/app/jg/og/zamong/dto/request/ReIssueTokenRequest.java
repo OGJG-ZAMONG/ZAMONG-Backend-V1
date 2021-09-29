42a3da962a4cdbdaa947201eb771be65523d1530
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-29 23:58:06 +0900

    ⚡️ :: (#41) <ReIssueTokenRequest> validation 설정

--- src/main/java/app/jg/og/zamong/dto/request/ReIssueTokenRequest.java
+++ src/main/java/app/jg/og/zamong/dto/request/ReIssueTokenRequest.java
@@ -2,11 +2,16 @@ package app.jg.og.zamong.dto.request;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReIssueTokenRequest {
 
+    @NotBlank
+    @Size(min = 6, max = 500)
     private String refreshToken;
 }
