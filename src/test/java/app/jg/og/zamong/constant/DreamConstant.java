7996f0261ca1f9cbfe5c005de78cc45b6e42d9a6
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-03 19:58:50 +0900

    ⚡️ :: (#35) <ShareDreamConstant> 공유 꿈 상수

--- src/test/java/app/jg/og/zamong/constant/DreamConstant.java
+++ src/test/java/app/jg/og/zamong/constant/DreamConstant.java
@@ -4,4 +4,9 @@ public class DreamConstant {
 
     public static final String TITLE = "title";
     public static final String CONTENT = "content";
+
+    public static final String QUALITY = "SO";
+    public static final String TYPE = "BST";
+    public static final Boolean IS_SHARED = false;
+    public static final Integer SLEEP_TIME = 4;
 }
