aa3ef21cdfcf891d26829878f500bedc502759ac
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-04 22:43:12 +0900

    ⚡️ :: (#48) <ShareDreamRepository>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/sharedream/ShareDreamRepository.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.entity.dream.sharedream;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface ShareDreamRepository extends CrudRepository<ShareDream, UUID> {
+}
