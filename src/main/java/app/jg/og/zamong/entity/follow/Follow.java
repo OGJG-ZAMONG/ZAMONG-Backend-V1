867f4b66e4889243a98a11fa25a3ad27f5ef3657
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-05 08:18:11 +0900

    :bookmark_tabs: :: (#45) <Follow> 팔로우 관계 테이블

---
+++ src/main/java/app/jg/og/zamong/entity/follow/Follow.java
@@ -0,0 +1,30 @@
+package app.jg.og.zamong.entity.follow;
+
+import app.jg.og.zamong.entity.user.User;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Follow {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "user_uuid", nullable = false)
+    private User following;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_uuid", nullable = false)
+    private User follower;
+}
