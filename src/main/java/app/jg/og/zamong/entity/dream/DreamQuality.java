bdbd9fe3ea0a28222d3737c6abfd7cd2ee36a190
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-04 17:42:59 +0900

    :recycle: :: (#35) <DreamQuality> HashMap 활용 find 속도 증가

--- src/main/java/app/jg/og/zamong/entity/dream/DreamQuality.java
+++ src/main/java/app/jg/og/zamong/entity/dream/DreamQuality.java
@@ -1,7 +1,15 @@
 package app.jg.og.zamong.entity.dream;
 
+import app.jg.og.zamong.exception.externalinfra.AttributeConvertFailedException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
@@ -14,4 +22,13 @@ public enum DreamQuality {
     WORST("WST");
 
     private final String code;
+
+    private static final Map<String, DreamQuality> qualities =
+            Collections.unmodifiableMap(Arrays.stream(DreamQuality.values())
+                    .collect(Collectors.toMap(DreamQuality::getCode, Function.identity())));
+
+    public static DreamQuality find(String code) {
+        return Optional.ofNullable(qualities.get(code))
+                .orElseThrow(() -> new AttributeConvertFailedException("Attribute Convert Failed " + code));
+    }
 }
--- src/main/java/app/jg/og/zamong/entity/dream/sharedream/converter/DreamQualityConverter.java
+++ src/main/java/app/jg/og/zamong/entity/dream/sharedream/converter/DreamQualityConverter.java
@@ -1,25 +1,20 @@
 package app.jg.og.zamong.entity.dream.sharedream.converter;
 
 import app.jg.og.zamong.entity.dream.DreamQuality;
-import app.jg.og.zamong.exception.externalinfra.AttributeConvertFailedException;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
-import java.util.Arrays;
 
 @Converter
 public class DreamQualityConverter implements AttributeConverter<DreamQuality, String> {
 
     @Override
     public String convertToDatabaseColumn(DreamQuality attribute) {
-        return attribute.name();
+        return attribute.getCode();
     }
 
     @Override
     public DreamQuality convertToEntityAttribute(String dbData) {
-        return Arrays.stream(DreamQuality.values())
-                .filter(dq -> dq.getCode().equals(dbData))
-                .findFirst()
-                .orElseThrow(() -> new AttributeConvertFailedException("Attribute Convert Failed " + dbData));
+        return DreamQuality.find(dbData);
     }
 }
