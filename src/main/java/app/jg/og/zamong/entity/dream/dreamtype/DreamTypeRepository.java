f91a7e3c3c82457b819192d5476ff57d4c6eabda
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-04 22:42:57 +0900

    :bookmark_tabs: :: (#48) <DreamTypeRepository>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/dreamtype/DreamTypeRepository.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.entity.dream.dreamtype;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface DreamTypeRepository extends CrudRepository<DreamType, UUID> {
+}
