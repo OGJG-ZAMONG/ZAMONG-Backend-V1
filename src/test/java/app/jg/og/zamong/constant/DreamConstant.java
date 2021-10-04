30601535d7cba5bcc73acb83d8be8f62f73981e5
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-04 23:35:57 +0900

    ⚡️ :: (#48) <DreamConstant> sleepDateTime

--- src/test/java/app/jg/og/zamong/constant/DreamConstant.java
+++ src/test/java/app/jg/og/zamong/constant/DreamConstant.java
@@ -2,6 +2,8 @@ package app.jg.og.zamong.constant;
 
 import app.jg.og.zamong.entity.dream.enums.DreamQuality;
 
+import java.util.Date;
+
 public class DreamConstant {
 
     public static final String TITLE = "title";
@@ -9,5 +11,6 @@ public class DreamConstant {
 
     public static final DreamQuality QUALITY = DreamQuality.SOSO;
     public static final Boolean IS_SHARED = false;
+    public static final Date SLEEP_DATE_TIME = new Date(System.currentTimeMillis());
     public static final Integer SLEEP_TIME = 4;
 }
