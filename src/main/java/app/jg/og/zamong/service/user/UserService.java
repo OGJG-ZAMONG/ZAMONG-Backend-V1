4a0b3f1e866cc4c7c54d1c33541d1c945d3e32a9
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-05 23:54:22 +0900

    ⚡️ :: (#53) <UserService> 팔로우 기능 구현

--- src/main/java/app/jg/og/zamong/service/user/UserService.java
+++ src/main/java/app/jg/og/zamong/service/user/UserService.java
@@ -1,8 +1,10 @@
 package app.jg.og.zamong.service.user;
 
+import app.jg.og.zamong.dto.response.FollowUserResponse;
 import app.jg.og.zamong.dto.response.UserInformationResponse;
 
 public interface UserService {
 
     UserInformationResponse queryUserInformation(String uuid);
+    FollowUserResponse followUser(String uuid, String followerUuid);
 }
--- src/main/java/app/jg/og/zamong/service/user/UserServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/user/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package app.jg.og.zamong.service.user;
 
+import app.jg.og.zamong.dto.request.FollowUserRequest;
+import app.jg.og.zamong.dto.response.FollowUserResponse;
 import app.jg.og.zamong.dto.response.UserInformationResponse;
 import app.jg.og.zamong.entity.dream.sharedream.ShareDreamRepository;
+import app.jg.og.zamong.entity.follow.Follow;
+import app.jg.og.zamong.entity.follow.FollowRepository;
 import app.jg.og.zamong.entity.user.User;
 import app.jg.og.zamong.entity.user.UserRepository;
 import app.jg.og.zamong.exception.business.UserNotFoundException;
@@ -16,6 +20,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ShareDreamRepository shareDreamRepository;
+    private final FollowRepository followRepository;
 
     @Override
     public UserInformationResponse queryUserInformation(String uuid) {
@@ -33,4 +38,20 @@ public class UserServiceImpl implements UserService {
                 .lucyCount(user.getLucyCount())
                 .build();
    }
+
+    @Override
+    public FollowUserResponse followUser(String uuid, String followerUuid) {
+        User user = userRepository.findByUuid(UUID.fromString(uuid))
+                .orElseThrow(() -> new UserNotFoundException("해당하는 유저를 찾을 수 없습니다"));
+        User follower = userRepository.findByUuid(UUID.fromString(followerUuid))
+                .orElseThrow(() -> new UserNotFoundException("해당하는 유저를 찾을 수 없습니다"));
+        followRepository.save(Follow.builder()
+                .following(user)
+                .follower(follower)
+                .build());
+        return FollowUserResponse.builder()
+                .userId(user.getUuid())
+                .followerId(follower.getUuid())
+                .build();
+    }
 }
