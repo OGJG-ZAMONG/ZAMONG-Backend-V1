6f57fd5d5e9af09ee91ee39b5911afc1b8bba4a5
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-05 09:09:42 +0900

    ⚡️ :: (#45) <ShareDreamRepository> findByUser 메서드 추가

--- src/main/java/app/jg/og/zamong/entity/dream/sharedream/ShareDreamRepository.java
+++ src/main/java/app/jg/og/zamong/entity/dream/sharedream/ShareDreamRepository.java
@@ -1,8 +1,11 @@
 package app.jg.og.zamong.entity.dream.sharedream;
 
+import app.jg.og.zamong.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ShareDreamRepository extends CrudRepository<ShareDream, UUID> {
+    List<ShareDream> findByUser(User user);
 }
