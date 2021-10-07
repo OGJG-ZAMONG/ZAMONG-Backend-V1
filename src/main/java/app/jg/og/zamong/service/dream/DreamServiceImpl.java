1b33ca869c8720063faf05d3d7909454e358ed40
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-07 20:15:21 +0900

    ⚡️ :: (#60) <DreamServie> 클라이언트에 맞게 기능 수정

--- src/main/java/app/jg/og/zamong/service/dream/DreamServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/dream/DreamServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -38,8 +39,8 @@ public class DreamServiceImpl implements DreamService {
                 .user(user)
                 .quality(request.getQuality())
                 .isShared(false)
-                .sleepDateTime(request.getSleepDateTime())
-                .sleepTime(request.getSleepTime())
+                .sleepDateTime(request.getSleepBeginDatetime())
+                .sleepTime((int) ChronoUnit.HOURS.between(request.getSleepBeginDatetime(), request.getSleepEndDatetime()))
                 .build()
         );
 
