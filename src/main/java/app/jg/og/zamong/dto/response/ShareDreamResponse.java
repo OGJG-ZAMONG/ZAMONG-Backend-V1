a32b4fa1ddc0549f48939c61f15c4c95304e2f6a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-09 20:00:25 +0900

    :bookmark_tabs: :: (#59) <ShareDreamResponse> 공유꿈 기본 응답 객체

---
+++ src/main/java/app/jg/og/zamong/dto/response/ShareDreamResponse.java
@@ -0,0 +1,32 @@
+package app.jg.og.zamong.dto.response;
+
+import app.jg.og.zamong.entity.dream.enums.DreamType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+public class ShareDreamResponse {
+
+    private final UUID uuid;
+
+    private final String defaultPostingImage;
+
+    private final String title;
+
+    private final String content;
+
+    private final Boolean isShared;
+
+    private final LocalDateTime createdAt;
+
+    private final LocalDateTime updatedAt;
+
+    private final Integer lucyCount;
+
+    private final List<DreamType> dreamTypes;
+}
