f62d655e303d95e50fbde278b09fcb9e88283e2f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-13 20:42:51 +0900

    :bookmark_tabs: :: (#71) <UnitTest>

---
+++ src/test/java/app/jg/og/zamong/service/UnitTest.java
@@ -0,0 +1,12 @@
+package app.jg.og.zamong.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+@Disabled
+public class UnitTest {
+}
