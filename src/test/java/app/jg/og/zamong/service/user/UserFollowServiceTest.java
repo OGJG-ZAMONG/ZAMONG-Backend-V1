70e191fd6c6b7bc974f8a2c0560361bc85f0879d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-13 20:42:33 +0900

    :bookmark_tabs: :: (#71) <UserFollowServiceTest>

---
+++ src/test/java/app/jg/og/zamong/service/user/UserFollowServiceTest.java
@@ -0,0 +1,65 @@
+package app.jg.og.zamong.service.user;
+
+import app.jg.og.zamong.dto.response.FollowUserResponse;
+import app.jg.og.zamong.entity.follow.Follow;
+import app.jg.og.zamong.entity.follow.FollowRepository;
+import app.jg.og.zamong.entity.user.User;
+import app.jg.og.zamong.entity.user.UserRepository;
+import app.jg.og.zamong.service.user.follow.UserFollowServiceImpl;
+import app.jg.og.zamong.util.UserBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+public class UserFollowServiceTest {
+
+    @InjectMocks
+    private UserFollowServiceImpl userFollowService;
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private FollowRepository followRepository;
+
+    static private User user;
+
+    @BeforeAll
+    static void setUp() {
+        user = UserBuilder.build();
+    }
+
+    @Test
+    void 유저_팔로우_성공() {
+        //given
+        User follower = User.builder().uuid(UUID.randomUUID()).build();
+        UUID uuid = user.getUuid();
+        UUID followerUuid = follower.getUuid();
+
+        given(userRepository.findByUuid(uuid)).willReturn(Optional.of(user));
+        given(userRepository.findByUuid(followerUuid)).willReturn(Optional.of(follower));
+        given(followRepository.save(any(Follow.class))).willReturn(Follow.builder()
+                .following(user)
+                .follower(follower)
+                .build());
+
+        //when
+        FollowUserResponse response = userFollowService.followUser(uuid.toString(), followerUuid.toString());
+
+        //then
+        assertThat(response.getUserId()).isEqualTo(uuid);
+        assertThat(response.getFollowerId()).isEqualTo(followerUuid);
+    }
+}
