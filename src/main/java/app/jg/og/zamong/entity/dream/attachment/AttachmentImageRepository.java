e0a5f3dbfe0fbbb2d86573d5be0015adf0f91e70
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-14 14:22:57 +0900

    :bookmark_tabs: :: (#73) <AttachmentImageRepository>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/attachment/AttachmentImageRepository.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.entity.dream.attachment;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface AttachmentImageRepository extends CrudRepository<AttachmentImage, UUID> {
+}
