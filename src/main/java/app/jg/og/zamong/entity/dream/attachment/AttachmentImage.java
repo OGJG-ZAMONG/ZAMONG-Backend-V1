d554d0290c85c4d47be68772aeb4c0089f3fac2e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-14 14:21:37 +0900

    ⚡️ :: (#73) <AttachmentImage> DynamicInsert

--- src/main/java/app/jg/og/zamong/entity/dream/attachment/AttachmentImage.java
+++ src/main/java/app/jg/og/zamong/entity/dream/attachment/AttachmentImage.java
@@ -4,6 +4,7 @@ import app.jg.og.zamong.entity.dream.Dream;
 import app.jg.og.zamong.entity.dream.attachment.converter.DreamTagConverter;
 import app.jg.og.zamong.entity.dream.enums.DreamTag;
 import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
 @Entity
 @Table(name = "attachment_image")
 public class AttachmentImage {
