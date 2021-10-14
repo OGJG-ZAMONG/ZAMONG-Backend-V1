5803fdb83dd83cad041ad2d8f54f4b8c2fb8971c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-14 15:23:10 +0900

    ⚡️ :: (#83) <FollowUserResponse> followDateTime 추가

--- src/main/java/app/jg/og/zamong/dto/response/FollowUserResponse.java
+++ src/main/java/app/jg/og/zamong/dto/response/FollowUserResponse.java
@@ -1,8 +1,10 @@
 package app.jg.og.zamong.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Builder
@@ -10,5 +12,9 @@ import java.util.UUID;
 public class FollowUserResponse implements Response {
 
     private final UUID userId;
+
     private final UUID followerId;
+
+    @JsonProperty("follow_datetime")
+    private final LocalDateTime followDateTime;
 }
