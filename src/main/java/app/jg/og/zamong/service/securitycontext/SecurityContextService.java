b087ac410074c27d37ce457b8b561b02f35f9389
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-16 21:48:21 +0900

    ⚡️ :: (#74) <SecurityContextService> getPrincipal 메서드 추가

--- src/main/java/app/jg/og/zamong/service/securitycontext/SecurityContextService.java
+++ src/main/java/app/jg/og/zamong/service/securitycontext/SecurityContextService.java
@@ -1,5 +1,8 @@
 package app.jg.og.zamong.service.securitycontext;
 
+import app.jg.og.zamong.security.auth.AuthenticationDetails;
+
 public interface SecurityContextService {
     String getName();
+    AuthenticationDetails getPrincipal();
 }
--- src/main/java/app/jg/og/zamong/service/securitycontext/SecurityContextServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/securitycontext/SecurityContextServiceImpl.java
@@ -1,5 +1,6 @@
 package app.jg.og.zamong.service.securitycontext;
 
+import app.jg.og.zamong.security.auth.AuthenticationDetails;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,10 @@ public class SecurityContextServiceImpl implements SecurityContextService {
     @Override
     public String getName() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @Override
+    public AuthenticationDetails getPrincipal() {
+        return (AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
