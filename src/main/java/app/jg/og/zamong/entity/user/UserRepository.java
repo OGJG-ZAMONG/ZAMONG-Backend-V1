5080cae85b4b1bd11307ad1e306e80e71c38dff0
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-16 20:39:40 +0900

    :coffin: :: (#74) <UserRepository> 사용하지 않는 메서드 삭제

--- src/main/java/app/jg/og/zamong/entity/user/UserRepository.java
+++ src/main/java/app/jg/og/zamong/entity/user/UserRepository.java
@@ -10,9 +10,7 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, UUID> {
-    Optional<User> findByUuid(UUID uuid);
     Optional<User> findById(String id);
-    Optional<User> findByEmail(String email);
     @Query("select u from User u where u.email = :id or u.id = :id")
     Optional<User> findByEmailOrId(@Param("id") String userId);
     Optional<User> findByEmailOrId(String email, String id);
