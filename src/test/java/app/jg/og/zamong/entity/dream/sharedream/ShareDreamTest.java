4c06df43378f3d3594870482450a9778779e28a6
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-17 08:39:07 +0900

    :bookmark_tabs: :: (#85) <ShareDreamTest> 도메인 로직 테스 트

---
+++ src/test/java/app/jg/og/zamong/entity/dream/sharedream/ShareDreamTest.java
@@ -0,0 +1,25 @@
+package app.jg.og.zamong.entity.dream.sharedream;
+
+import app.jg.og.zamong.exception.business.AlreadySharedException;
+import app.jg.og.zamong.exception.business.BusinessException;
+import app.jg.og.zamong.util.ShareDreamBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+public class ShareDreamTest {
+
+    @Test
+    void 이미_공유한_꿈_예외() {
+        ShareDream shareDream = ShareDreamBuilder.build(null);
+        shareDream.setIsShared(true);
+
+        try {
+            shareDream.doShare();
+            fail("No Error");
+        } catch (BusinessException e) {
+            assertThat(e).isInstanceOf(AlreadySharedException.class);
+        }
+    }
+}
