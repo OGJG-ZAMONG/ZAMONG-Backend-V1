8d50b7f9d60cef077e3fbdcaa8ec9e1fdbe6586a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-17 18:24:33 +0900

    ⚡️ :: (#87) <ShareDreamResponse> ShareDraem 변환 메서드 추가

--- src/main/java/app/jg/og/zamong/dto/response/ShareDreamResponse.java
+++ src/main/java/app/jg/og/zamong/dto/response/ShareDreamResponse.java
@@ -1,6 +1,7 @@
 package app.jg.og.zamong.dto.response;
 
 import app.jg.og.zamong.entity.dream.enums.DreamType;
+import app.jg.og.zamong.entity.dream.sharedream.ShareDream;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter
@@ -29,4 +31,20 @@ public class ShareDreamResponse {
 
     @JsonProperty("share_datetime")
     private final LocalDateTime shareDateTime;
+
+    public static ShareDreamResponse of(ShareDream shareDream) {
+        return ShareDreamResponse.builder()
+                .uuid(shareDream.getUuid())
+                .title(shareDream.getTitle())
+                .profile(shareDream.getUser().getProfile())
+                .isShared(shareDream.getIsShared())
+                .lucyCount(shareDream.getLucyCount())
+                .dreamTypes(shareDream.getDreamTypes()
+                        .stream()
+                        .map(dt -> dt.toCode())
+                        .collect(Collectors.toList()))
+                .defaultPostingImage(shareDream.getDefaultImage())
+                .shareDateTime(shareDream.getShareDateTime())
+                .build();
+    }
 }
