08421448eb6743eaaa9ff7960e21c4bf53a69830
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-17 14:58:20 +0900

    :test_tube: :: (#86) <ShareDreamTest> 공유되지 않은 꿈 예외처리 테스트

--- src/test/java/app/jg/og/zamong/entity/dream/sharedream/ShareDreamTest.java
+++ src/test/java/app/jg/og/zamong/entity/dream/sharedream/ShareDreamTest.java
@@ -2,6 +2,7 @@ package app.jg.og.zamong.entity.dream.sharedream;
 
 import app.jg.og.zamong.exception.business.AlreadySharedException;
 import app.jg.og.zamong.exception.business.BusinessException;
+import app.jg.og.zamong.exception.business.NotSharedException;
 import app.jg.og.zamong.util.ShareDreamBuilder;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +21,19 @@ public class ShareDreamTest {
             fail("No Error");
         } catch (BusinessException e) {
             assertThat(e).isInstanceOf(AlreadySharedException.class);
+        }
+    }
+
+    @Test()
+    void 공유되지_않은_꿈_예외() {
+        ShareDream shareDream = ShareDreamBuilder.build(null);
+        shareDream.setIsShared(false);
+
+        try {
+            shareDream.cancelShare();
+            fail("No Error");
+        } catch (BusinessException e) {
+            assertThat(e).isInstanceOf(NotSharedException.class);
         }
     }
 }
