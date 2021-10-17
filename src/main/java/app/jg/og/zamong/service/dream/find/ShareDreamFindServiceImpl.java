0b16c8bfd7d218e30f3f21969c15e097162c4850
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-18 01:42:47 +0900

    ⚡️ :: (#87) <ShareDreamFindService> 내가 작성한 꿈 기능구현

--- src/main/java/app/jg/og/zamong/service/dream/find/ShareDreamFindServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/dream/find/ShareDreamFindServiceImpl.java
@@ -1,5 +1,7 @@
 package app.jg.og.zamong.service.dream.find;
 
+import app.jg.og.zamong.dto.response.ShareDreamGroupResponse;
+import app.jg.og.zamong.dto.response.ShareDreamResponse;
 import app.jg.og.zamong.dto.response.SharedDreamGroupResponse;
 import app.jg.og.zamong.dto.response.SharedDreamResponse;
 import app.jg.og.zamong.entity.dream.sharedream.ShareDream;
@@ -31,6 +33,30 @@ public class ShareDreamFindServiceImpl implements ShareDreamFindService {
                 .map(SharedDreamResponse::of).toList();
 
         return SharedDreamGroupResponse.builder()
+                .shareDreams(shareDreamGroup)
+                .totalPage(shareDreamPage.getTotalPages())
+                .totalSize(shareDreamPage.getTotalElements())
+                .build();
+    }
+
+    @Override
+    public ShareDreamGroupResponse queryMyShareDreams(int page, int size) {
+        User user = securityContextService.getPrincipal().getUser();
+
+        Pageable request = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        Page<ShareDream> shareDreamPage = shareDreamRepository.findByUser(user, request);
+
+        List<ShareDreamResponse> shareDreamGroup = shareDreamPage
+                .map(sd -> ShareDreamResponse.builder()
+                        .uuid(sd.getUuid())
+                        .title(sd.getTitle())
+                        .defaultPostingImage(sd.getDefaultImage())
+                        .isShared(sd.getIsShared())
+                        .createdAt(sd.getCreatedAt())
+                        .build())
+                .toList();
+
+        return ShareDreamGroupResponse.builder()
                 .shareDreams(shareDreamGroup)
                 .totalPage(shareDreamPage.getTotalPages())
                 .totalSize(shareDreamPage.getTotalElements())
