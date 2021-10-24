b99cad7755a9a758bb886d166a803cb79c6f7bd7
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-24 16:42:53 +0900

    :test_tube: :: (#104) <AuthControllerTest> 아이디 중복확인 성공 테스트

---
+++ src/test/java/app/jg/og/zamong/controller/AuthControllerTest.java
@@ -0,0 +1,49 @@
+package app.jg.og.zamong.controller;
+
+import app.jg.og.zamong.constant.UserConstant;
+import app.jg.og.zamong.dto.request.CheckIdDuplicationRequest;
+import app.jg.og.zamong.entity.user.User;
+import app.jg.og.zamong.entity.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthControllerTest extends IntegrationTest {
+
+    private User user;
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .name(UserConstant.NAME)
+                .email(UserConstant.EMAIL)
+                .id(UserConstant.ID)
+                .password(passwordEncoder.encode(UserConstant.PASSWORD))
+                .build();
+    }
+
+    @Test
+    @Transactional
+    void 아이디중복확인_성공() throws Exception {
+        CheckIdDuplicationRequest request = CheckIdDuplicationRequest.builder()
+                .id(user.getId())
+                .build();
+
+        mockMvc.perform(post("/auth/user-id/duplicate")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+}
