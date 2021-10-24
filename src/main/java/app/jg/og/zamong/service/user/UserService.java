981fce5013c1df855d47702f816dd9fbce04d923
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-24 17:22:37 +0900

    ⚡️ :: (#104) <UserServcie> 아이디 변경

--- src/main/java/app/jg/og/zamong/service/user/UserService.java
+++ src/main/java/app/jg/og/zamong/service/user/UserService.java
@@ -1,5 +1,6 @@
 package app.jg.og.zamong.service.user;
 
+import app.jg.og.zamong.dto.request.CheckIdDuplicationRequest;
 import app.jg.og.zamong.dto.response.UserInformationResponse;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,4 +8,5 @@ public interface UserService {
     UserInformationResponse queryUserInformation(String uuid);
     UserInformationResponse queryMyInformation();
     void modifyProfile(MultipartFile file);
+    void modifyUserId(CheckIdDuplicationRequest request);
 }
--- src/main/java/app/jg/og/zamong/service/user/UserServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/user/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package app.jg.og.zamong.service.user;
 
+import app.jg.og.zamong.dto.request.CheckIdDuplicationRequest;
 import app.jg.og.zamong.dto.response.UserInformationResponse;
 import app.jg.og.zamong.entity.dream.sharedream.ShareDreamRepository;
 import app.jg.og.zamong.entity.user.User;
 import app.jg.og.zamong.entity.user.UserRepository;
 import app.jg.og.zamong.entity.user.profile.Profile;
 import app.jg.og.zamong.entity.user.profile.ProfileRepository;
+import app.jg.og.zamong.exception.business.UserIdentityDuplicationException;
 import app.jg.og.zamong.exception.business.UserNotFoundException;
 import app.jg.og.zamong.service.file.FileSaveService;
 import app.jg.og.zamong.service.securitycontext.SecurityContextService;
@@ -56,5 +58,18 @@ public class UserServiceImpl implements UserService {
                 .host(host)
                 .path(path)
                 .build());
+    }
+
+    @Override
+    @Transactional
+    public void modifyUserId(CheckIdDuplicationRequest request) {
+        User user = securityContextService.getPrincipal().getUser();
+
+        userRepository.findById(request.getId())
+                .ifPresent((u) -> {
+                    throw new UserIdentityDuplicationException("이미 사용중인 아이디입니다");
+                });
+
+        user.setId(request.getId());
     }
 }
