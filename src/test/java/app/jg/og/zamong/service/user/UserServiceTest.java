1892306ce7b3987535c738463c90ecd284a4a04e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-24 17:07:26 +0900

    :test_tube: :: (#104) <UserServiceTest> 프로필 수정 성공

--- src/test/java/app/jg/og/zamong/service/user/UserServiceTest.java
+++ src/test/java/app/jg/og/zamong/service/user/UserServiceTest.java
@@ -1,12 +1,15 @@
 package app.jg.og.zamong.service.user;
 
+import app.jg.og.zamong.constant.UserConstant;
 import app.jg.og.zamong.dto.response.UserInformationResponse;
 import app.jg.og.zamong.entity.dream.sharedream.ShareDream;
 import app.jg.og.zamong.entity.dream.sharedream.ShareDreamRepository;
 import app.jg.og.zamong.entity.user.User;
 import app.jg.og.zamong.entity.user.UserRepository;
+import app.jg.og.zamong.entity.user.profile.ProfileRepository;
 import app.jg.og.zamong.security.auth.AuthenticationDetails;
 import app.jg.og.zamong.service.UnitTest;
+import app.jg.og.zamong.service.file.FileSaveService;
 import app.jg.og.zamong.service.securitycontext.SecurityContextService;
 import app.jg.og.zamong.util.ShareDreamBuilder;
 import app.jg.og.zamong.util.UserBuilder;
@@ -14,13 +17,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class UserServiceTest extends UnitTest {
 
@@ -33,6 +42,10 @@ public class UserServiceTest extends UnitTest {
     private ShareDreamRepository shareDreamRepository;
     @Mock
     private SecurityContextService securityContextService;
+    @Mock
+    private FileSaveService fileSaveService;
+    @Mock
+    private ProfileRepository profileRepository;
 
     static private User user;
 
@@ -75,5 +88,23 @@ public class UserServiceTest extends UnitTest {
         //then
         assertThat(response.getEmail()).isEqualTo(user.getEmail());
         assertThat(response.getShareDreamCount()).isEqualTo(shareDreamCount);
+    }
+
+    @Test
+    void 프로필_수정_성공() {
+        //given
+        MultipartFile file = new MockMultipartFile("file_name", "file_content".getBytes(StandardCharsets.UTF_8));
+
+        String directory = "user";
+
+        given(fileSaveService.queryHostName()).willReturn(UserConstant.PROFILE_HOST);
+        given(fileSaveService.saveFile(file, directory)).willReturn(UserConstant.PROFILE_PATH);
+        given(securityContextService.getPrincipal()).willReturn(new AuthenticationDetails(user));
+
+        //when
+        userService.modifyProfile(file);
+
+        //then
+        verify(profileRepository, times(1)).save(any());
     }
 }
