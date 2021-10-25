5ae4a0d25c718fe4735eb69158d8a3ac3916bbc5
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-25 12:07:21 +0900

    :test_tube: :: (#105) <AuthControllerTest> signup 400

--- src/test/java/app/jg/og/zamong/controller/AuthControllerTest.java
+++ src/test/java/app/jg/og/zamong/controller/AuthControllerTest.java
@@ -2,8 +2,12 @@ package app.jg.og.zamong.controller;
 
 import app.jg.og.zamong.constant.UserConstant;
 import app.jg.og.zamong.dto.request.CheckIdDuplicationRequest;
+import app.jg.og.zamong.dto.request.SignUpUserRequest;
+import app.jg.og.zamong.entity.redis.authenticationcode.AuthenticationCode;
+import app.jg.og.zamong.entity.redis.authenticationcode.AuthenticationCodeRepository;
 import app.jg.og.zamong.entity.user.User;
 import app.jg.og.zamong.entity.user.UserRepository;
+import app.jg.og.zamong.exception.ErrorCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AuthControllerTest extends IntegrationTest {
@@ -22,6 +27,8 @@ public class AuthControllerTest extends IntegrationTest {
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationCodeRepository authenticationCodeRepository;
 
     @BeforeEach
     void setUp() {
@@ -61,5 +68,28 @@ public class AuthControllerTest extends IntegrationTest {
                 .content(objectMapper.writeValueAsString(request))
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Transactional
+    void auth_signup_400_1() throws Exception {
+        String name = "정지우";
+        String id = user.getId();
+        String email = user.getEmail();
+        String password = "NewPassword@1";
+        String authenticationCode = "000000";
+
+        SignUpUserRequest request = SignUpUserRequest.builder()
+                .name(name)
+                .id(id)
+                .email(email)
+                .password(password)
+                .authenticationCode(authenticationCode)
+                .build();
+
+        mockMvc.perform(post("/auth/signup")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest()).andExpect(jsonPath("message").value(ErrorCode.USER_IDENTITY_DUPLICATION.getMessage()));
     }
 }
