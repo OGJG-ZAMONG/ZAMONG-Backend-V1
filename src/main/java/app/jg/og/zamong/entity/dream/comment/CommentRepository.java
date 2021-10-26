99b8855aca3eb045e34f7cd9a01a8faf58d3f895
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-26 20:19:42 +0900

    :bookmark_tabs: :: (#103) <CommentRepository>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/comment/CommentRepository.java
@@ -0,0 +1,10 @@
+package app.jg.og.zamong.entity.dream.comment;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CommentRepository extends CrudRepository<Comment, UUID> {
+}
