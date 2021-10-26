dbbcc9bf68b4e3b386388888b17f6b3b99cdbad2
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-26 20:19:23 +0900

    :bookmark_tabs: :: (#103) <Comment>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/comment/Comment.java
@@ -0,0 +1,49 @@
+package app.jg.og.zamong.entity.dream.comment;
+
+import app.jg.og.zamong.entity.dream.Dream;
+import app.jg.og.zamong.entity.user.User;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID uuid;
+
+    @Column(length = 100)
+    private String content;
+
+    @Column(columnDefinition = "tinyint(1)")
+    private Boolean isChecked;
+
+    @Column(name = "datetime", columnDefinition = "timestamp")
+    private LocalDateTime dateTime;
+
+    @OneToMany(mappedBy = "pComment")
+    private List<Comment> comments;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_uuid", nullable = false)
+    private Comment pComment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_uuid", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dream_uuid", nullable = false)
+    private Dream dream;
+}
