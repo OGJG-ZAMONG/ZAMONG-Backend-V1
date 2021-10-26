74888d536115039eb8da59cda5f5b84ce66747e9
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-26 15:26:19 +0900

    :bookmark_tabs: :: (#109) <UserControllerTest> /user 통합테스트

---
+++ src/test/java/app/jg/og/zamong/controller/UserControllerTest.java
@@ -0,0 +1,49 @@
+package app.jg.og.zamong.controller;
+
+import app.jg.og.zamong.constant.UserConstant;
+import app.jg.og.zamong.dto.request.LoginUserRequest;
+import app.jg.og.zamong.entity.user.User;
+import app.jg.og.zamong.entity.user.UserRepository;
+import app.jg.og.zamong.entity.user.profile.Profile;
+import app.jg.og.zamong.entity.user.profile.ProfileRepository;
+import app.jg.og.zamong.service.user.auth.UserAuthenticationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class UserControllerTest extends IntegrationTest {
+
+    private User user;
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ProfileRepository profileRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserAuthenticationService userAuthenticationService;
+
+    @BeforeEach
+    void setUp() {
+        user = userRepository.save(User.builder()
+                .name(UserConstant.NAME)
+                .email(UserConstant.EMAIL)
+                .id(UserConstant.ID)
+                .lucyCount(0)
+                .password(passwordEncoder.encode(UserConstant.PASSWORD))
+                .build());
+        Profile profile = profileRepository.save(Profile.builder()
+                .uuid(user.getUuid())
+                .build());
+        user.setProfile(profile);
+        userRepository.save(user);
+    }
+}
