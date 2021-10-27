b8e3008c1aa5f21bed4c0a3004fd6a37b773747e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-27 10:52:55 +0900

    :bookmark_tabs: :: (#103) <DreamCommentResponse>

---
+++ src/main/java/app/jg/og/zamong/dto/response/DreamCommentResponse.java
@@ -0,0 +1,17 @@
+package app.jg.og.zamong.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+public class DreamCommentResponse implements Response {
+
+    private final UUID uuid;
+
+    private final String content;
+
+    private final UserInformationResponse user;
+}
