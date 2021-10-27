9ff4ace1d9c7a48cce95dafcddf5ba4fef63321a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-27 10:53:42 +0900

    :bookmark_tabs: :: (#103) <DreamCommentService>

---
+++ src/main/java/app/jg/og/zamong/service/dream/comment/DreamCommentService.java
@@ -0,0 +1,9 @@
+package app.jg.og.zamong.service.dream.comment;
+
+import app.jg.og.zamong.dto.request.dream.DreamCommentRequest;
+import app.jg.og.zamong.dto.response.DreamCommentResponse;
+
+public interface DreamCommentService {
+
+    DreamCommentResponse createDream(String dreamId, DreamCommentRequest request);
+}
