5fb00a5c800f6d981bcdfa4533cc65fcbb90fd0b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-28 08:48:58 +0900

    :bookmark_tabs: :: (#117) <ShareDreamTimeTableResponseV2> 리스트로 된 객체

---
+++ src/main/java/app/jg/og/zamong/dto/response/ShareDreamTimeTableResponseV2.java
@@ -0,0 +1,35 @@
+package app.jg.og.zamong.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+public class ShareDreamTimeTableResponseV2 implements Response {
+
+    private final List<ShareDreamResponse> timetables;
+
+    @Builder
+    @Getter
+    public static class ShareDreamResponse {
+
+        private final LocalDate date;
+
+        private final UUID uuid;
+
+        private final String defaultPostingImage;
+
+        private final String profile;
+
+        private final String title;
+
+        private final LocalDateTime createdAt;
+
+        private final Boolean isShared;
+    }
+}
