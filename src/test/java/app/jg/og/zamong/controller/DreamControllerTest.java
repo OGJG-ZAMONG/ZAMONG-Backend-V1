e8d68434f9b0736d4c7c2a38722fd2b9d0b895c4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-27 11:17:37 +0900

    :bookmark_tabs: :: (#103) <DreamControllerTest>

---
+++ src/test/java/app/jg/og/zamong/controller/DreamControllerTest.java
@@ -0,0 +1,73 @@
+package app.jg.og.zamong.controller;
+
+import app.jg.og.zamong.constant.UserConstant;
+import app.jg.og.zamong.dto.request.LoginUserRequest;
+import app.jg.og.zamong.dto.request.dream.DreamCommentRequest;
+import app.jg.og.zamong.entity.dream.Dream;
+import app.jg.og.zamong.entity.dream.DreamRepository;
+import app.jg.og.zamong.entity.dream.comment.CommentRepository;
+import app.jg.og.zamong.entity.user.User;
+import app.jg.og.zamong.entity.user.UserRepository;
+import app.jg.og.zamong.entity.user.profile.Profile;
+import app.jg.og.zamong.service.user.auth.UserAuthenticationService;
+import app.jg.og.zamong.util.ShareDreamBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class DreamControllerTest extends IntegrationTest {
+
+    private User user;
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private DreamRepository dreamRepository;
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private UserAuthenticationService userAuthenticationService;
+
+    @BeforeEach
+    void setUp() {
+        user = userRepository.save(User.builder()
+                .name(UserConstant.NAME)
+                .email(UserConstant.EMAIL)
+                .id(UserConstant.ID)
+                .password(passwordEncoder.encode(UserConstant.PASSWORD))
+                .build());
+        Profile profile = profileRepository.save(Profile.builder()
+                .uuid(user.getUuid())
+                .build());
+        user.setProfile(profile);
+        userRepository.save(user);
+    }
+
+    @AfterEach
+    void deleteAl() {
+        userRepository.deleteAll();
+        dreamRepository.deleteAll();
+        commentRepository.deleteAll();
+    }
+
+    private String loginUser() {
+        return userAuthenticationService.loginUser(LoginUserRequest.builder()
+                .userIdentity(user.getEmail())
+                .password(UserConstant.PASSWORD)
+                .build()).getAccessToken();
+    }
+
+    private String createBearerToken(String token) {
+        return "Bearer " + token;
+    }
+}
