6beff85d954b3754f0a9bc961d583401a00bd7ac
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-31 19:55:00 +0900

    :bookmark_tabs: :: (#113) <RecommendType> 댓글 추천 enum

---
+++ src/main/java/app/jg/og/zamong/entity/dream/comment/recommend/RecommendType.java
@@ -0,0 +1,33 @@
+package app.jg.og.zamong.entity.dream.comment.recommend;
+
+import app.jg.og.zamong.exception.externalinfra.AttributeConvertFailedException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+public enum RecommendType {
+
+    DISLIKE("DLK", "싫어요"),
+    LIKE("LK", "좋아요");
+
+    private final String code;
+
+    private final String value;
+
+    private static final Map<String, RecommendType> qualities =
+            Collections.unmodifiableMap(Arrays.stream(RecommendType.values())
+                    .collect(Collectors.toMap(RecommendType::getCode, Function.identity())));
+
+    public static RecommendType find(String code) {
+        return Optional.ofNullable(qualities.get(code))
+                .orElseThrow(() -> new AttributeConvertFailedException("Attribute Convert Failed " + code));
+    }
+}
