b256f8d5a409c097457ca1cefae57963d2bac1b3
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-31 18:27:14 +0900

    :test_tube: :: (#115) <ShareDreamServiceTest> 공유꿈 사진 수정 테스트

--- src/test/java/app/jg/og/zamong/service/dream/ShareDreamServiceTest.java
+++ src/test/java/app/jg/og/zamong/service/dream/ShareDreamServiceTest.java
@@ -3,6 +3,7 @@ package app.jg.og.zamong.service.dream;
 import app.jg.og.zamong.dto.request.dream.sharedream.*;
 import app.jg.og.zamong.dto.response.CreateShareDreamResponse;
 import app.jg.og.zamong.dto.response.DoShareDreamResponse;
+import app.jg.og.zamong.entity.dream.attachment.AttachmentImage;
 import app.jg.og.zamong.entity.dream.attachment.AttachmentImageRepository;
 import app.jg.og.zamong.entity.dream.dreamtype.DreamTypeRepository;
 import app.jg.og.zamong.entity.dream.enums.*;
@@ -11,6 +12,7 @@ import app.jg.og.zamong.entity.user.User;
 import app.jg.og.zamong.entity.user.UserRepository;
 import app.jg.og.zamong.service.UnitTest;
 import app.jg.og.zamong.service.dream.share.ShareDreamServiceImpl;
+import app.jg.og.zamong.service.file.FileSaveService;
 import app.jg.og.zamong.service.securitycontext.SecurityContextService;
 import app.jg.og.zamong.util.*;
 import org.junit.jupiter.api.Test;
@@ -30,6 +32,8 @@ public class ShareDreamServiceTest extends UnitTest {
 
     @Mock
     private SecurityContextService securityContextService;
+    @Mock
+    private FileSaveService fileSaveService;
     @Mock
     private ShareDreamRepository shareDreamRepository;
     @Mock
@@ -96,6 +100,28 @@ public class ShareDreamServiceTest extends UnitTest {
         assertThat(shareDream.getTitle()).isEqualTo(patchedTitle);
         assertThat(shareDream.getContent()).isEqualTo(patchedContent);
         assertThat(shareDream.getQuality()).isEqualTo(pathedQuality);
+    }
+
+    @Test
+    void 공유꿈_사진_수정_성공() {
+        //given
+        ShareDream shareDream = ShareDreamBuilder.build(null);
+
+        String uploadHost = "uploadHost";
+        String uploadPath = "uploadPath";
+
+        given(shareDreamRepository.findById(shareDream.getUuid())).willReturn(Optional.of(shareDream));
+        given(fileSaveService.queryHostName()).willReturn(uploadHost);
+        given(fileSaveService.saveFile(any(), any())).willReturn(uploadPath);
+
+        AttachmentImage attachmentImage = shareDream.getAttachmentImages().get(0);
+
+        //when
+        shareDreamService.patchShareDreamImage(shareDream.getUuid().toString(), null);
+
+        //then
+        assertThat(attachmentImage.getHost()).isEqualTo(uploadHost);
+        assertThat(attachmentImage.getPath()).isEqualTo(uploadPath);
     }
 
     @Test
