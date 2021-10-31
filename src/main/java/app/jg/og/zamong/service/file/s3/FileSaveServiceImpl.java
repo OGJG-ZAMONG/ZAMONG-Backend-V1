1a9a075ebf054f3a976fb34b1a4de92e6fe07436
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-31 17:40:01 +0900

    ⚡️ :: (#115) <FileSaveService> 파일 이름 중복 제거

--- src/main/java/app/jg/og/zamong/service/file/s3/FileSaveServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/file/s3/FileSaveServiceImpl.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -36,7 +37,7 @@ public class FileSaveServiceImpl implements FileSaveService {
     }
 
     private String saveFile(File file, String directory) {
-        String fileName = directory + "/" + file.getName();
+        String fileName = directory + "/" + UUID.randomUUID() + file.getName().substring(file.getName().lastIndexOf("."));
         String uploadFilePath = putS3(file, fileName);
         file.delete();
         return uploadFilePath;
