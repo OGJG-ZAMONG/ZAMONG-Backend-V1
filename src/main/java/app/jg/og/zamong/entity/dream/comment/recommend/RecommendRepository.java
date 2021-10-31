f5eb48e0650ed22a9aca55f9b12620c6a5f6dbe6
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-10-31 19:55:45 +0900

    :bookmark_tabs: :: (#113) <RecommendRepository>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/comment/recommend/RecommendRepository.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.entity.dream.comment.recommend;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface RecommendRepository extends CrudRepository<Recommend, UUID> {
+}
