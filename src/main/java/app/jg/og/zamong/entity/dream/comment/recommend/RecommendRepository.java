c93839c1d7c45dbb9b70b6372e11d03bfa8723d8
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-01 09:14:30 +0900

    ⚡️ :: (#114) <RecommendRepository> findByCommentAndUser

--- src/main/java/app/jg/og/zamong/entity/dream/comment/recommend/RecommendRepository.java
+++ src/main/java/app/jg/og/zamong/entity/dream/comment/recommend/RecommendRepository.java
@@ -4,10 +4,12 @@ import app.jg.og.zamong.entity.dream.comment.Comment;
 import app.jg.og.zamong.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RecommendRepository extends CrudRepository<Recommend, UUID> {
 
+    Optional<Recommend> findByCommentAndUser(Comment comment, User user);
     Integer countAllByCommentAndRecommendType(Comment comment, RecommendType recommendType);
     Boolean existsByCommentAndUserAndRecommendType(Comment comment, User user, RecommendType recommendType);
 }
