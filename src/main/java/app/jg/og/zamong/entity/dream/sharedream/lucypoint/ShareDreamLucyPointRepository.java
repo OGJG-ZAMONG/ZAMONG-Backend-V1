15bf813fb9811cab21588446f540099e89d93fae
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-01 11:10:55 +0900

    ⚡️ :: (#124) <SharedreamLucyPointRepository> findByUserAndShareDream

--- src/main/java/app/jg/og/zamong/entity/dream/sharedream/lucypoint/ShareDreamLucyPointRepository.java
+++ src/main/java/app/jg/og/zamong/entity/dream/sharedream/lucypoint/ShareDreamLucyPointRepository.java
@@ -1,10 +1,15 @@
 package app.jg.og.zamong.entity.dream.sharedream.lucypoint;
 
+import app.jg.og.zamong.entity.dream.sharedream.ShareDream;
+import app.jg.og.zamong.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ShareDreamLucyPointRepository extends CrudRepository<ShareDreamLucyPoint, UUID> {
+
+    Optional<ShareDreamLucyPoint> findByUserAndShareDream(User user, ShareDream shareDream);
 }
