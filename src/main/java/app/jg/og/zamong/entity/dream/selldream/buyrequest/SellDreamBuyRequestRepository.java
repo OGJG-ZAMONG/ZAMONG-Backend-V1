c947e3245676ac990c1ea5a90826821869f49496
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-02 14:53:32 +0900

    :bookmark_tabs: :: (#130) <SellDreamBuyReqeustRepository>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/selldream/buyrequest/SellDreamBuyRequestRepository.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.entity.dream.selldream.buyrequest;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface SellDreamBuyRequestRepository extends CrudRepository<SellDreamBuyRequest, UUID> {
+}
