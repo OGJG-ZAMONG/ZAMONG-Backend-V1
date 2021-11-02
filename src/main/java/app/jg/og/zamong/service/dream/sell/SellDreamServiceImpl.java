d6ecc8c8feea1e0e657995dfe2aca95b701af333
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-02 15:09:04 +0900

    ⚡️ :: (#130) <SellDreamService> 꿈 구매요청 기능

--- src/main/java/app/jg/og/zamong/service/dream/sell/SellDreamServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/dream/sell/SellDreamServiceImpl.java
@@ -3,6 +3,7 @@ package app.jg.og.zamong.service.dream.sell;
 import app.jg.og.zamong.dto.request.dream.selldream.SellDreamCostRequest;
 import app.jg.og.zamong.dto.request.dream.selldream.SellDreamRequest;
 import app.jg.og.zamong.dto.response.CreateDreamResponse;
+import app.jg.og.zamong.dto.response.DoSellRequestDreamResponse;
 import app.jg.og.zamong.entity.dream.attachment.AttachmentImage;
 import app.jg.og.zamong.entity.dream.attachment.AttachmentImageRepository;
 import app.jg.og.zamong.entity.dream.dreamtype.DreamType;
@@ -10,6 +11,8 @@ import app.jg.og.zamong.entity.dream.dreamtype.DreamTypeRepository;
 import app.jg.og.zamong.entity.dream.enums.DreamTag;
 import app.jg.og.zamong.entity.dream.selldream.SellDream;
 import app.jg.og.zamong.entity.dream.selldream.SellDreamRepository;
+import app.jg.og.zamong.entity.dream.selldream.buyrequest.SellDreamBuyRequest;
+import app.jg.og.zamong.entity.dream.selldream.buyrequest.SellDreamBuyRequestRepository;
 import app.jg.og.zamong.entity.user.User;
 import app.jg.og.zamong.exception.business.DreamNotFoundException;
 import app.jg.og.zamong.service.securitycontext.SecurityContextService;
@@ -26,6 +29,7 @@ public class SellDreamServiceImpl implements SellDreamService {
     private final SellDreamRepository sellDreamRepository;
     private final AttachmentImageRepository attachmentImageRepository;
     private final DreamTypeRepository dreamTypeRepository;
+    private final SellDreamBuyRequestRepository sellDreamBuyRequestRepository;
 
     private final SecurityContextService securityContextService;
 
@@ -57,6 +61,25 @@ public class SellDreamServiceImpl implements SellDreamService {
                 .uuid(sellDream.getUuid())
                 .createdAt(sellDream.getCreatedAt())
                 .updatedAt(sellDream.getUpdatedAt())
+                .build();
+    }
+
+    @Override
+    public DoSellRequestDreamResponse doSellRequestDream(String uuid) {
+        User user = securityContextService.getPrincipal().getUser();
+
+        SellDream sellDream = sellDreamRepository.findById(UUID.fromString(uuid))
+                .orElseThrow(() -> new DreamNotFoundException("해당하는 꿈을 찾을 수 없습니다"));
+
+        sellDreamBuyRequestRepository.save(SellDreamBuyRequest.builder()
+                .user(user)
+                .sellDream(sellDream)
+                .isAccept(false)
+                .build());
+
+        return DoSellRequestDreamResponse.builder()
+                .userUuid(user.getUuid())
+                .sellDreamUuid(sellDream.getUuid())
                 .build();
     }
 
