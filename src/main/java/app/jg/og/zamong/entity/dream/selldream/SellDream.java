1d91bb8032142239793a1e363b58498880c6fc00
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-02 09:54:05 +0900

    :bookmark_tabs: :: (#130) <SellDream>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/selldream/SellDream.java
@@ -0,0 +1,17 @@
+package app.jg.og.zamong.entity.dream.selldream;
+
+import app.jg.og.zamong.entity.dream.Dream;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class SellDream extends Dream {
+
+    private Integer cost;
+}
