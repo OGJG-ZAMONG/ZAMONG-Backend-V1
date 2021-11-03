146132f66f0babe99f05e4526021663d9a2cc391
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-03 14:17:43 +0900

    ⚡️ :: (#141) <ShareDreamFindService> 루시순 정렬

--- src/main/java/app/jg/og/zamong/service/dream/find/ShareDreamFindService.java
+++ src/main/java/app/jg/og/zamong/service/dream/find/ShareDreamFindService.java
@@ -5,7 +5,7 @@ import app.jg.og.zamong.dto.response.*;
 public interface ShareDreamFindService {
 
     ShareDreamInformationResponse queryShareDreamInformation(String uuid);
-    SharedDreamGroupResponse queryShareDreams(int page, int size);
+    SharedDreamGroupResponse queryShareDreams(int page, int size, String sort);
     ShareDreamGroupResponse queryMyShareDreams(int page, int size);
     ShareDreamGroupResponse queryFollowShareDreams(int page, int size);
     ShareDreamTimeTableResponse queryMyShareDreamTimeTable(int year, int month);
