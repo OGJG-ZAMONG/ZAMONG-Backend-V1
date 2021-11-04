85234a7baa5c8d424a8b3a822d180b3dbe5df11c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-04 09:30:49 +0900

    ⚡️ :: (#146) <SellDrem> status 컬럼 추가

--- src/main/java/app/jg/og/zamong/entity/dream/selldream/SellDream.java
+++ src/main/java/app/jg/og/zamong/entity/dream/selldream/SellDream.java
@@ -1,9 +1,13 @@
 package app.jg.og.zamong.entity.dream.selldream;
 
 import app.jg.og.zamong.entity.dream.Dream;
+import app.jg.og.zamong.entity.dream.enums.SalesStatus;
+import app.jg.og.zamong.entity.dream.selldream.converter.SalesStatusConverter;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 
 @Getter
@@ -15,4 +19,8 @@ public class SellDream extends Dream {
 
     @Setter
     private Integer cost;
+
+    @Convert(converter = SalesStatusConverter.class)
+    @Column(length = 4, columnDefinition = "char(4)")
+    private SalesStatus status;
 }
