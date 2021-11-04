fc71d07dc9b7c4f39dee4572bdca40880dbbde37
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-05 00:35:04 +0900

    ⚡️ :: (#138) <SellDreamFindService> 판매중인 꿈 목록

---
+++ src/main/java/app/jg/og/zamong/service/dream/sell/find/SellDreamFindService.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.service.dream.sell.find;
+
+import app.jg.og.zamong.dto.response.dream.selldream.SellDreamGroupResponse;
+
+public interface SellDreamFindService {
+
+    SellDreamGroupResponse queryPendingSellDreams(int page, int size);
+}
---
+++ src/main/java/app/jg/og/zamong/service/dream/sell/find/SellDreamFindServiceImpl.java
@@ -0,0 +1,47 @@
+package app.jg.og.zamong.service.dream.sell.find;
+
+import app.jg.og.zamong.dto.response.dream.selldream.SellDreamGroupResponse;
+import app.jg.og.zamong.dto.response.dream.selldream.SellDreamResponse;
+import app.jg.og.zamong.entity.dream.dreamtype.DreamType;
+import app.jg.og.zamong.entity.dream.enums.SalesStatus;
+import app.jg.og.zamong.entity.dream.selldream.SellDream;
+import app.jg.og.zamong.entity.dream.selldream.SellDreamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SellDreamFindServiceImpl implements SellDreamFindService {
+
+    private final SellDreamRepository sellDreamRepository;
+
+    @Override
+    public SellDreamGroupResponse queryPendingSellDreams(int page, int size) {
+        Pageable request = PageRequest.of(page, size, Sort.by("updatedAt").descending());
+        Page<SellDream> sellDreamPage = sellDreamRepository.findByStatus(SalesStatus.PENDING, request);
+
+        List<SellDreamResponse> sellDreamGroup = sellDreamPage
+                .map(sd -> SellDreamResponse.builder()
+                        .uuid(sd.getUuid())
+                        .title(sd.getTitle())
+                        .defaultPostingImage(sd.getDefaultImage())
+                        .updatedAt(sd.getUpdatedAt())
+                        .dreamTypes(sd.getDreamTypes().stream().map(DreamType::getCode).collect(Collectors.toList()))
+                        .cost(sd.getCost())
+                        .build())
+                .toList();
+
+        return SellDreamGroupResponse.builder()
+                .sellDreams(sellDreamGroup)
+                .totalPage(sellDreamPage.getTotalPages())
+                .totalSize(sellDreamPage.getTotalElements())
+                .build();
+    }
+}
