d02d1113742bc1786076b4dfe968d94cfa0bec7e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-07 03:22:52 +0900

    ⚡️ :: (#152) <InterpretationDreamRepository> List 반환 findAll

--- src/main/java/app/jg/og/zamong/entity/dream/interpretationdream/interpretation/InterpretationRepository.java
+++ src/main/java/app/jg/og/zamong/entity/dream/interpretationdream/interpretation/InterpretationRepository.java
@@ -2,7 +2,10 @@ package app.jg.og.zamong.entity.dream.interpretationdream.interpretation;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InterpretationRepository extends CrudRepository<Interpretation, UUID> {
+
+    List<Interpretation> findAll();
 }
