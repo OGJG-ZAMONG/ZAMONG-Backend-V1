d5011b3ceba4dcf65b5f91c51916a6bf8a08342b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-07 03:21:26 +0900

    :bookmark_tabs: :: (#152) <InterpretationDreamService>

---
+++ src/main/java/app/jg/og/zamong/service/dream/interpretation/InterpretationDreamService.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.service.dream.interpretation;
+
+import app.jg.og.zamong.dto.response.dream.interpretationdream.InterpretationDreamCategoryResponse;
+
+public interface InterpretationDreamService {
+
+    InterpretationDreamCategoryResponse queryInterpretationDreamCategory();
+}
