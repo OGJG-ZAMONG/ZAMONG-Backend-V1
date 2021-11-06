9a5d2c959a834d2d944843436e9680bd3ca30e6c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-07 03:34:49 +0900

    ⚡️ :: (#152) <InterpretationDreamService> 기본 해몽 결과

--- src/main/java/app/jg/og/zamong/service/dream/interpretation/InterpretationDreamService.java
+++ src/main/java/app/jg/og/zamong/service/dream/interpretation/InterpretationDreamService.java
@@ -1,8 +1,12 @@
 package app.jg.og.zamong.service.dream.interpretation;
 
+import app.jg.og.zamong.dto.response.Response;
 import app.jg.og.zamong.dto.response.dream.interpretationdream.InterpretationDreamCategoryResponse;
+
+import java.util.UUID;
 
 public interface InterpretationDreamService {
 
     InterpretationDreamCategoryResponse queryInterpretationDreamCategory();
+    Response queryInterpretation(String uuid);
 }
--- src/main/java/app/jg/og/zamong/service/dream/interpretation/InterpretationDreamServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/dream/interpretation/InterpretationDreamServiceImpl.java
@@ -1,12 +1,16 @@
 package app.jg.og.zamong.service.dream.interpretation;
 
+import app.jg.og.zamong.dto.response.Response;
+import app.jg.og.zamong.dto.response.StringResponse;
 import app.jg.og.zamong.dto.response.dream.interpretationdream.InterpretationDreamCategoryResponse;
 import app.jg.og.zamong.entity.dream.interpretationdream.interpretation.Interpretation;
 import app.jg.og.zamong.entity.dream.interpretationdream.interpretation.InterpretationRepository;
+import app.jg.og.zamong.exception.business.DreamNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,5 +33,13 @@ public class InterpretationDreamServiceImpl implements InterpretationDreamServic
         return InterpretationDreamCategoryResponse.builder()
                 .interpretationDreams(interpretationDreams)
                 .build();
+    }
+
+    @Override
+    public Response queryInterpretation(String uuid) {
+        Interpretation interpretation = interpretationRepository.findById(UUID.fromString(uuid))
+                .orElseThrow(() -> new DreamNotFoundException("해몽을 찾을 수 없습니다"));
+
+        return new StringResponse(interpretation.getInterpretation());
     }
 }
