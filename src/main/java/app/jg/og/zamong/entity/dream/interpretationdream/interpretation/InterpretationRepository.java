b7b4647b13b32fd8824fe1a225a890d9cf0585f2
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-06 23:56:24 +0900

    :bookmark_tabs: :: (#152) <InterpretationRepository>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/interpretationdream/interpretation/InterpretationRepository.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.entity.dream.interpretationdream.interpretation;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface InterpretationRepository extends CrudRepository<Interpretation, UUID> {
+}
