d35a0d1b5afac94fa34e1029ac4a4215a834dc96
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-07 13:03:06 +0900

    :bookmark_tabs: :: (#151) <InterpretationDream>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/interpretationdream/InterpretationDream.java
@@ -0,0 +1,18 @@
+package app.jg.og.zamong.entity.dream.interpretationdream;
+
+import app.jg.og.zamong.entity.dream.Dream;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class InterpretationDream extends Dream {
+}
