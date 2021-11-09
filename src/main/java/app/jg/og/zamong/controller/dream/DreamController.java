735b0cc763f08359ef92beb5d3db3c520957ec8e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-09 20:11:40 +0900

    :coffin: :: (#134) <DreamController> /sell 분리

--- src/main/java/app/jg/og/zamong/controller/dream/DreamController.java
+++ src/main/java/app/jg/og/zamong/controller/dream/DreamController.java
@@ -6,21 +6,15 @@ import app.jg.og.zamong.dto.request.dream.DreamContentRequest;
 import app.jg.og.zamong.dto.request.dream.DreamTitleRequest;
 import app.jg.og.zamong.dto.request.dream.DreamTypesRequest;
 import app.jg.og.zamong.dto.request.dream.interpretationdream.InterpretationDreamRequest;
-import app.jg.og.zamong.dto.request.dream.selldream.SellDreamCostRequest;
-import app.jg.og.zamong.dto.request.dream.selldream.SellDreamRequest;
 import app.jg.og.zamong.dto.response.ResponseBody;
 import app.jg.og.zamong.service.dream.DreamService;
 import app.jg.og.zamong.service.dream.comment.DreamCommentService;
 import app.jg.og.zamong.service.dream.interpretation.InterpretationDreamService;
-import app.jg.og.zamong.service.dream.sell.SellDreamService;
-import app.jg.og.zamong.service.dream.sell.find.SellDreamFindService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.websocket.server.PathParam;
 
 @RequiredArgsConstructor
 @RestController
@@ -51,54 +45,6 @@ public class DreamController {
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("dream-uuid") String uuid) {
         dreamService.removeDream(uuid);
-    }
-
-    private final SellDreamService sellDreamService;
-
-    @PostMapping("/sell")
-    public ResponseBody sell(@Valid @RequestBody SellDreamRequest request) {
-        return ResponseBody.of(sellDreamService.createSellDream(request), HttpStatus.CREATED.value());
-    }
-
-    @PostMapping("/sell/{dream-uuid}/done")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void sellDone(@PathVariable("dream-uuid") String uuid) {
-        sellDreamService.doneSellDream(uuid);
-    }
-
-    @DeleteMapping("/sell/{dream-uuid}")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void sellCancel(@PathVariable("dream-uuid") String uuid) {
-        sellDreamService.cancelSellDream(uuid);
-    }
-
-    @PatchMapping("/sell/cost/{dream-uuid}")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void cost(@PathVariable("dream-uuid") String uuid, @Valid @RequestBody SellDreamCostRequest request) {
-        sellDreamService.patchSellDreamCost(uuid, request);
-    }
-
-    @PostMapping("/sell/{dream-uuid}/request")
-    public ResponseBody sellRequest(@PathVariable("dream-uuid") String uuid) {
-        return ResponseBody.of(sellDreamService.doSellRequestDream(uuid), HttpStatus.OK.value());
-    }
-
-    private final SellDreamFindService sellDreamFindService;
-
-    @GetMapping("/sell/continue")
-    public ResponseBody sellContinue(
-            @PathParam("page") int page,
-            @Max(50) @PathParam("size") int size
-    ) {
-        return ResponseBody.listOf(sellDreamFindService.queryPendingSellDreams(page, size), HttpStatus.OK.value());
-    }
-
-    @GetMapping("/sell/close")
-    public ResponseBody sellClose(
-            @PathParam("page") int page,
-            @Max(50) @PathParam("size") int size
-    ) {
-        return ResponseBody.listOf(sellDreamFindService.queryClosedSellDream(page, size), HttpStatus.OK.value());
     }
 
     private final InterpretationDreamService interpretationDreamService;
