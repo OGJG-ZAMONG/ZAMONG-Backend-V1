aea7ae4e8f72dbbd576feac99e9bd164544b33f8
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-11 20:20:40 +0900

    :bookmark_tabs: :: (#161) <InterpretationDreamResponse>

---
+++ src/main/java/app/jg/og/zamong/dto/response/dream/interpretationdream/InterpretationDreamResponse.java
@@ -0,0 +1,37 @@
+package app.jg.og.zamong.dto.response.dream.interpretationdream;
+
+import app.jg.og.zamong.entity.dream.enums.DreamType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+public class InterpretationDreamResponse {
+
+    private final UUID uuid;
+
+    private final String defaultPostingImage;
+
+    private final String title;
+
+    private final List<DreamType> dreamTypes;
+
+    private final LocalDateTime updatedAt;
+
+    private final User user;
+
+    @Builder
+    @Getter
+    public static class User {
+
+        private final UUID uuid;
+
+        private final String id;
+
+        private final String profile;
+    }
+}
