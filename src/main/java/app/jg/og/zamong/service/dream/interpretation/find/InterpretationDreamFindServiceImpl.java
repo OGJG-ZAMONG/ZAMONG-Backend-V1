72e47f42910e207c9f1a460484507c7324535bcd
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-11 20:25:16 +0900

    ⚡️ :: (#161) <InterpretationDreamFindService> interpretationDreamResponseOf 메서드

--- src/main/java/app/jg/og/zamong/service/dream/interpretation/find/InterpretationDreamFindServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/dream/interpretation/find/InterpretationDreamFindServiceImpl.java
@@ -2,6 +2,7 @@ package app.jg.og.zamong.service.dream.interpretation.find;
 
 import app.jg.og.zamong.dto.response.dream.interpretationdream.InterpretationDreamGroupResponse;
 import app.jg.og.zamong.dto.response.dream.interpretationdream.InterpretationDreamResponse;
+import app.jg.og.zamong.entity.dream.dreamtype.DreamType;
 import app.jg.og.zamong.entity.dream.interpretationdream.InterpretationDream;
 import app.jg.og.zamong.entity.dream.interpretationdream.InterpretationDreamRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +28,20 @@ public class InterpretationDreamFindServiceImpl implements InterpretationDreamFi
         Page<InterpretationDream> interpretationDreamPage = interpretationDreamRepository.findAll(request);
 
         return null;
+    }
+
+    private InterpretationDreamResponse interpretationDreamResponseOf(InterpretationDream interpretationDream) {
+        return InterpretationDreamResponse.builder()
+                .uuid(interpretationDream.getUuid())
+                .defaultPostingImage(interpretationDream.getDefaultImage())
+                .title(interpretationDream.getTitle())
+                .dreamTypes(interpretationDream.getDreamTypes().stream().map(DreamType::getCode).collect(Collectors.toList()))
+                .updatedAt(interpretationDream.getUpdatedAt())
+                .user(InterpretationDreamResponse.User.builder()
+                        .uuid(interpretationDream.getUser().getUuid())
+                        .id(interpretationDream.getUser().getId())
+                        .profile(interpretationDream.getUser().getProfile())
+                        .build())
+                .build();
     }
 }
