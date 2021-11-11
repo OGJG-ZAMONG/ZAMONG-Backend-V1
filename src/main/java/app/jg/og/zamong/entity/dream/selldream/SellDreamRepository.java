46799fca6387bf6cb74f4339549f3685de45f6c5
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-11 19:09:52 +0900

    ⚡️ :: (#162) <SellDreamRepository> SellDreamSearchRepository 상속

--- src/main/java/app/jg/og/zamong/entity/dream/selldream/SellDreamRepository.java
+++ src/main/java/app/jg/og/zamong/entity/dream/selldream/SellDreamRepository.java
@@ -1,6 +1,7 @@
 package app.jg.og.zamong.entity.dream.selldream;
 
 import app.jg.og.zamong.entity.dream.enums.SalesStatus;
+import app.jg.og.zamong.entity.dream.selldream.search.SellDreamSearchRepository;
 import app.jg.og.zamong.entity.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +10,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 import java.util.UUID;
 
-public interface SellDreamRepository extends PagingAndSortingRepository<SellDream, UUID> {
+public interface SellDreamRepository extends PagingAndSortingRepository<SellDream, UUID>, SellDreamSearchRepository {
     Page<SellDream> findByStatus(SalesStatus status, Pageable pageable);
     Page<SellDream> findByStatusAndUser(SalesStatus status, User user, Pageable pageable);
     Page<SellDream> findByUserAndStatusIn(User user, List<SalesStatus> statuses, Pageable pageable);
