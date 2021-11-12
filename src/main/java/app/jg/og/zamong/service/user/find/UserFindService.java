052bd8f4460fe623b181559067adb0786e519d91
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-12 13:30:06 +0900

    :bookmark_tabs: :: (#163) <UserFindService>

---
+++ src/main/java/app/jg/og/zamong/service/user/find/UserFindService.java
@@ -0,0 +1,9 @@
+package app.jg.og.zamong.service.user.find;
+
+import app.jg.og.zamong.dto.response.user.UserInformationResponse;
+
+public interface UserFindService {
+
+    UserInformationResponse queryUserInformation(String uuid);
+    UserInformationResponse queryMyInformation();
+}
