992850100a777753204b597032538c59eda879e7
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-12 13:40:49 +0900

    :coffin: :: (#163) <FollowGroupRespons> FollowerGroupResponse -> UserResponse

--- src/main/java/app/jg/og/zamong/dto/response/user/follow/FollowingGroupResponse.java
+++ src/main/java/app/jg/og/zamong/dto/response/user/follow/FollowingGroupResponse.java
@@ -1,37 +1,19 @@
 package app.jg.og.zamong.dto.response.user.follow;
 
 import app.jg.og.zamong.dto.response.Response;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import app.jg.og.zamong.dto.response.user.UserResponse;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 @Builder
 @Getter
 public class FollowingGroupResponse implements Response {
 
-    private final List<FollowingResponse> followings;
+    private final List<UserResponse> followings;
 
     private final Integer totalPage;
 
     private final Long totalSize;
-
-    @Builder
-    @Getter
-    public static class FollowingResponse {
-
-        private final UUID uuid;
-
-        private final String id;
-
-        private final String profile;
-
-        @JsonProperty("follow_datetime")
-        private final LocalDateTime followDateTime;
-
-        private final Boolean isFollowing;
-    }
 }
