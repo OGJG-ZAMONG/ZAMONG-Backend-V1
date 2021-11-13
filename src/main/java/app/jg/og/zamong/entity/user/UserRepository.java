babce3ce2d9270e3d8a4b0ef4a1724d3f35bbffe
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-13 22:43:41 +0900

    :coffin: :: (#163) <UserRepository> UserSearchRepository 상속

--- src/main/java/app/jg/og/zamong/entity/user/UserRepository.java
+++ src/main/java/app/jg/og/zamong/entity/user/UserRepository.java
@@ -1,5 +1,6 @@
 package app.jg.og.zamong.entity.user;
 
+import app.jg.og.zamong.entity.user.search.UserSearchRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends CrudRepository<User, UUID>, UserSearchRepository {
     Optional<User> findById(String id);
     @Query("select u from User u where u.email = :id or u.id = :id")
     Optional<User> findByEmailOrId(@Param("id") String userId);
