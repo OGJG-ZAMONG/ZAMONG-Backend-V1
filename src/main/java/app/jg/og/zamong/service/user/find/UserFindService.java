fc0646308ff320ed8090fac0dd76c09913f7b50d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-13 22:57:26 +0900

    ⚡️ :: (#163) <UserFindService> 유저 검색

--- src/main/java/app/jg/og/zamong/service/user/find/UserFindService.java
+++ src/main/java/app/jg/og/zamong/service/user/find/UserFindService.java
@@ -1,9 +1,12 @@
 package app.jg.og.zamong.service.user.find;
 
+import app.jg.og.zamong.dto.response.user.UserGroupResponse;
 import app.jg.og.zamong.dto.response.user.UserInformationResponse;
 
 public interface UserFindService {
 
     UserInformationResponse queryUserInformation(String uuid);
     UserInformationResponse queryMyInformation();
+
+    UserGroupResponse searchUsers(String query);
 }
--- src/main/java/app/jg/og/zamong/service/user/find/UserFindServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/user/find/UserFindServiceImpl.java
@@ -1,6 +1,8 @@
 package app.jg.og.zamong.service.user.find;
 
+import app.jg.og.zamong.dto.response.user.UserGroupResponse;
 import app.jg.og.zamong.dto.response.user.UserInformationResponse;
+import app.jg.og.zamong.dto.response.user.UserResponse;
 import app.jg.og.zamong.entity.dream.sharedream.ShareDreamRepository;
 import app.jg.og.zamong.entity.user.User;
 import app.jg.og.zamong.entity.user.UserRepository;
@@ -9,7 +11,9 @@ import app.jg.og.zamong.service.securitycontext.SecurityContextService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,20 @@ public class UserFindServiceImpl implements UserFindService {
 
         Integer shareDreamCount = shareDreamRepository.findByUser(user).size();
         return UserInformationResponse.of(user, shareDreamCount);
+    }
+
+    @Override
+    public UserGroupResponse searchUsers(String query) {
+        List<User> users = userRepository.findAllByUserId(query);
+
+        return UserGroupResponse.builder()
+                .users(users.stream()
+                        .map(user -> UserResponse.builder()
+                                .uuid(user.getUuid())
+                                .id(user.getId())
+                                .profile(user.getProfile())
+                                .build())
+                        .collect(Collectors.toList()))
+                .build();
     }
 }
