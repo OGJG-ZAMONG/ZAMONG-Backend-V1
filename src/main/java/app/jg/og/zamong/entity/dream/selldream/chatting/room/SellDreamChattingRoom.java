848423970a5cc690f1051eba8f3a74142e683145
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-15 08:12:56 +0900

    :bookmark_tabs: :: (#133) <SellDreamChattingRoom>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/selldream/chatting/room/SellDreamChattingRoom.java
@@ -0,0 +1,37 @@
+package app.jg.og.zamong.entity.dream.selldream.chatting.room;
+
+import app.jg.og.zamong.entity.dream.selldream.SellDream;
+import app.jg.og.zamong.entity.user.User;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class SellDreamChattingRoom {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID uuid;
+
+    private String lastChat;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_uuid", nullable = false)
+    private User seller;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_uuid", nullable = false)
+    private User customer;
+
+    @ManyToOne
+    @JoinColumn(name = "sell_dream_uuid", nullable = false)
+    private SellDream sellDream;
+}
