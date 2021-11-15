7e672baa42ec291bedd35aac508cb6e9c421f1dc
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-15 12:11:48 +0900

    ⚡️ :: (#171) <DreamService> 이미지 수정

--- src/main/java/app/jg/og/zamong/service/dream/DreamService.java
+++ src/main/java/app/jg/og/zamong/service/dream/DreamService.java
@@ -3,12 +3,15 @@ package app.jg.og.zamong.service.dream;
 import app.jg.og.zamong.dto.request.dream.DreamContentRequest;
 import app.jg.og.zamong.dto.request.dream.DreamTitleRequest;
 import app.jg.og.zamong.dto.request.dream.DreamTypesRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface DreamService {
 
     void patchDreamTitle(String uuid, DreamTitleRequest request);
     void patchDreamContent(String uuid, DreamContentRequest request);
     void patchDreamTypes(String uuid, DreamTypesRequest request);
+
+    void patchDreamImage(String uuid, MultipartFile file);
 
     void removeDream(String uuid);
 }
--- src/main/java/app/jg/og/zamong/service/dream/DreamServiceImpl.java
+++ src/main/java/app/jg/og/zamong/service/dream/DreamServiceImpl.java
@@ -5,12 +5,15 @@ import app.jg.og.zamong.dto.request.dream.DreamTitleRequest;
 import app.jg.og.zamong.dto.request.dream.DreamTypesRequest;
 import app.jg.og.zamong.entity.dream.Dream;
 import app.jg.og.zamong.entity.dream.DreamRepository;
+import app.jg.og.zamong.entity.dream.attachment.AttachmentImage;
 import app.jg.og.zamong.entity.dream.dreamtype.DreamType;
 import app.jg.og.zamong.entity.dream.dreamtype.DreamTypeRepository;
 import app.jg.og.zamong.exception.business.DreamNotFoundException;
+import app.jg.og.zamong.service.file.FileSaveService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
@@ -20,6 +23,8 @@ public class DreamServiceImpl implements DreamService {
 
     private final DreamTypeRepository dreamTypeRepository;
     private final DreamRepository dreamRepository;
+
+    private final FileSaveService fileSaveService;
 
     @Override
     @Transactional
@@ -52,6 +57,21 @@ public class DreamServiceImpl implements DreamService {
                         .dream(dream)
                         .code(dt)
                         .build())));
+    }
+
+    @Override
+    @Transactional
+    public void patchDreamImage(String uuid, MultipartFile file) {
+        Dream dream = dreamRepository.findById(UUID.fromString(uuid))
+                .orElseThrow(() -> new DreamNotFoundException("해당하는 꿈을 찾을 수 없습니다"));
+
+        String host = fileSaveService.queryHostName();
+        String path = fileSaveService.saveFile(file, "dream/share");
+
+        AttachmentImage attachmentImage = dream.getAttachmentImages().get(0);
+
+        attachmentImage.setHost(host);
+        attachmentImage.setPath(path);
     }
 
     @Override
