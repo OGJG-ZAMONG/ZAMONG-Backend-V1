871ec7c52b000fc5b14aa7a48cbedeaa3ac63cd1
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-18 00:21:55 +0900

    ⚡️ :: (#178) <CommentRepository> findByDreamOrderByDateTimeDesc

--- src/main/java/app/jg/og/zamong/entity/dream/comment/CommentRepository.java
+++ src/main/java/app/jg/og/zamong/entity/dream/comment/CommentRepository.java
@@ -1,10 +1,14 @@
 package app.jg.og.zamong.entity.dream.comment;
 
+import app.jg.og.zamong.entity.dream.Dream;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, UUID> {
+
+    List<Comment> findByDreamOrderByDateTimeDesc(Dream dream);
 }
