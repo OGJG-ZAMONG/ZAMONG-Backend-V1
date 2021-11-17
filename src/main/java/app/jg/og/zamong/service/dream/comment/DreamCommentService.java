ecdf25af2cfd14c3f09c6a7ff9bcd5390c51ba19
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-18 01:00:46 +0900

    ⚡️ :: (#177) <DreamCommentService> 댓글 총 개수

--- src/main/java/app/jg/og/zamong/service/dream/comment/DreamCommentService.java
+++ src/main/java/app/jg/og/zamong/service/dream/comment/DreamCommentService.java
@@ -2,6 +2,7 @@ package app.jg.og.zamong.service.dream.comment;
 
 import app.jg.og.zamong.dto.request.DreamCommentRecommendRequest;
 import app.jg.og.zamong.dto.request.dream.DreamCommentRequest;
+import app.jg.og.zamong.dto.response.NumberResponse;
 import app.jg.og.zamong.dto.response.dream.comment.DreamCommendGroupResponse;
 import app.jg.og.zamong.dto.response.dream.comment.DreamCommentResponse;
 
@@ -12,6 +13,8 @@ public interface DreamCommentService {
     DreamCommendGroupResponse queryDreamReComment(String uuid);
     void doCommentRecommend(String uuid, DreamCommentRecommendRequest request);
     void patchCommentContent(String uuid, DreamCommentRequest  request);
+
+    NumberResponse queryCountOfComment(String uuid);
 
     void checkDreamComment(String uuid);
 }
