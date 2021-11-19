f490a09b7aec1f586b4c6c3b8697097fb097e016
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-19 13:30:35 +0900

    :bookmark_tabs: :: (#183) <SellDreamRequestGroupResponse>

---
+++ src/main/java/app/jg/og/zamong/dto/response/dream/selldream/SellDreamRequestGroupResponse.java
@@ -0,0 +1,33 @@
+package app.jg.og.zamong.dto.response.dream.selldream;
+
+import app.jg.og.zamong.dto.response.Response;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+public class SellDreamRequestGroupResponse implements Response {
+
+    private final List<Request> requests;
+
+    @Builder
+    @Getter
+    public static class Request {
+
+        private final UUID uuid;
+
+        private final UUID userUuid;
+
+        private final String id;
+
+        private final String profile;
+
+        @JsonProperty("request_datetime")
+        private final LocalDateTime requestDateTime;
+    }
+}
