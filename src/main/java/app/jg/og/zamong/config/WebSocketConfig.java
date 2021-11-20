d6876be140ea4e080fffdeba23af1ee591befbe7
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-20 23:30:09 +0900

    ⚡️ :: (#183) <WebSocketConfig> webSocket cors 허용

--- src/main/java/app/jg/og/zamong/config/WebSocketConfig.java
+++ src/main/java/app/jg/og/zamong/config/WebSocketConfig.java
@@ -1,19 +1,30 @@
 package app.jg.og.zamong.config;
 
+import app.jg.og.zamong.socket.StompHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final StompHandler stompHandler;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins(
+                "http://127.0.0.1:5500",
+                "http://localhost:3000",
+                "http://localhost:8080",
+                "http://www.zamong.app.s3-website.ap-northeast-2.amazonaws.com"
+        ).withSockJS();
     }
 
     @Override
