8d25831e05875ee94b07bf17f99e5f589390d99e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-22 10:27:10 +0900

    ⚡️ :: (#183) <ChatResponse> Response 구현

--- src/main/java/app/jg/og/zamong/dto/response/dream/selldream/chatting/ChatResponse.java
+++ src/main/java/app/jg/og/zamong/dto/response/dream/selldream/chatting/ChatResponse.java
@@ -1,5 +1,6 @@
 package app.jg.og.zamong.dto.response.dream.selldream.chatting;
 
+import app.jg.og.zamong.dto.response.Response;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,7 +9,7 @@ import java.util.UUID;
 
 @Builder
 @Getter
-public class ChatResponse {
+public class ChatResponse implements Response {
 
     private final UUID uuid;
 
