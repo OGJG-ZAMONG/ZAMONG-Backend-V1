9a195e2ed28c0312829aba966cf48177a2a4b9c3
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-22 11:26:28 +0900

    ⚡️ :: (#183) <SellDremaChattingRepository> findByRoom

--- src/main/java/app/jg/og/zamong/entity/dream/selldream/chatting/chat/SellDreamChattingRepository.java
+++ src/main/java/app/jg/og/zamong/entity/dream/selldream/chatting/chat/SellDreamChattingRepository.java
@@ -1,8 +1,13 @@
 package app.jg.og.zamong.entity.dream.selldream.chatting.chat;
 
-import org.springframework.data.repository.CrudRepository;
+import app.jg.og.zamong.entity.dream.selldream.chatting.room.SellDreamChattingRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface SellDreamChattingRepository extends CrudRepository<SellDreamChatting, UUID> {
+public interface SellDreamChattingRepository extends PagingAndSortingRepository<SellDreamChatting, UUID> {
+
+    Page<SellDreamChatting> findByRoom(SellDreamChattingRoom room, Pageable pageable);
 }
