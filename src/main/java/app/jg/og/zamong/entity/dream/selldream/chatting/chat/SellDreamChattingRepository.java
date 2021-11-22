22e6cda9f326a356b2afaf704d37ad3d063b7a3a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-22 10:27:26 +0900

    :bookmark_tabs: :: (#183) <SellDreamChattingRepository>

---
+++ src/main/java/app/jg/og/zamong/entity/dream/selldream/chatting/chat/SellDreamChattingRepository.java
@@ -0,0 +1,8 @@
+package app.jg.og.zamong.entity.dream.selldream.chatting.chat;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface SellDreamChattingRepository extends CrudRepository<SellDreamChatting, UUID> {
+}
