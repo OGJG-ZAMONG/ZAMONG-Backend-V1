c07eec54e6f9aef93d9f939e8f0fefcd86aa8bea
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-22 10:26:24 +0900

    :coffin: :: (#183) <WebSocketConfig> stomp handler 삭제

--- src/main/java/app/jg/og/zamong/config/WebSocketConfig.java
+++ src/main/java/app/jg/og/zamong/config/WebSocketConfig.java
@@ -1,10 +1,8 @@
 package app.jg.og.zamong.config;
 
-import app.jg.og.zamong.socket.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -14,8 +12,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final StompHandler stompHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -31,10 +27,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(stompHandler);
     }
 }
