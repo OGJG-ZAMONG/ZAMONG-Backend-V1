7ef7999b5d9ce016210a429edfe174bfa2cd71a4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-25 01:45:00 +0900

    ⚡️ :: (#190) <MailService> sendSimpleEmail

--- src/main/java/app/jg/og/zamong/service/mail/MailService.java
+++ src/main/java/app/jg/og/zamong/service/mail/MailService.java
@@ -1,7 +1,9 @@
 package app.jg.og.zamong.service.mail;
 
 import app.jg.og.zamong.dto.request.SendMailRequest;
+import app.jg.og.zamong.dto.request.SendSimpleMailRequest;
 
 public interface MailService {
     String sendEmail(SendMailRequest request);
+    void sendSimpleEmail(SendSimpleMailRequest request);
 }
