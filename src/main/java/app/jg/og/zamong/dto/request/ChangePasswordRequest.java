752587035b970a2811edf97f2530a154da445c86
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-25 01:59:34 +0900

    ⚡️ :: (#190) <ChangePasswordRequest> changePasswordToken

--- src/main/java/app/jg/og/zamong/dto/request/ChangePasswordRequest.java
+++ src/main/java/app/jg/og/zamong/dto/request/ChangePasswordRequest.java
@@ -18,4 +18,7 @@ public class ChangePasswordRequest {
     @NotNull(message = "비밀번호를 반드시 입력해야 합니다")
     @Size(max = 24, message = "너무 긴 비밀번호입니다")
     private String newPassword;
+
+    @NotNull
+    private String changePasswordToken;
 }
