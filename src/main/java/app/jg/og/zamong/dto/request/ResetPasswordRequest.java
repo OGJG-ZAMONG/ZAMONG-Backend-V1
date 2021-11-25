4aa09a1637669ea31e719b06981cc40d3c98283b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-25 09:10:43 +0900

    :bookmark_tabs: :: (#190) <ResestPasswordRequest> 비밀번호 재설정 요청 객체

---
+++ src/main/java/app/jg/og/zamong/dto/request/ResetPasswordRequest.java
@@ -0,0 +1,22 @@
+package app.jg.og.zamong.dto.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ResetPasswordRequest {
+
+    @NotNull(message = "비밀번호를 반드시 입력해야 합니다")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "비밀번호는 영문 대소문자와 숫자 특수기호 포함 8자 이상이어야 합니다")
+    @Size(max = 24, message = "너무 긴 비밀번호입니다")
+    private String newPassword;
+
+    @NotNull
+    private String changePasswordToken;
+}
