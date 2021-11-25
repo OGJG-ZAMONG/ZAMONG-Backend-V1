febe488bf5f0e9b4020f3b7c8fcd863df6c11e34
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-25 09:08:23 +0900

    :bookmark_tabs: :: (#190) <ChangePasswordRequest> changePassToken 속성 삭제

--- src/main/java/app/jg/og/zamong/dto/request/ChangePasswordRequest.java
+++ src/main/java/app/jg/og/zamong/dto/request/ChangePasswordRequest.java
@@ -3,6 +3,7 @@ package app.jg.og.zamong.dto.request;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Builder
@@ -12,13 +13,12 @@ import javax.validation.constraints.Size;
 public class ChangePasswordRequest {
 
     @NotNull(message = "비밀번호를 반드시 입력해야 합니다")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "비밀번호는 영문 대소문자와 숫자 특수기호 포함 8자 이상이어야 합니다")
     @Size(max = 24, message = "너무 긴 비밀번호입니다")
     private String oldPassword;
 
     @NotNull(message = "비밀번호를 반드시 입력해야 합니다")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "비밀번호는 영문 대소문자와 숫자 특수기호 포함 8자 이상이어야 합니다")
     @Size(max = 24, message = "너무 긴 비밀번호입니다")
     private String newPassword;
-
-    @NotNull
-    private String changePasswordToken;
 }
