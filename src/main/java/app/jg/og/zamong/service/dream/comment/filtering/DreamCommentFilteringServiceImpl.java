bf0b1de65556f701314bbba7af38681b9c24c689
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-30 01:43:50 +0900

    ⚡️ :: (#185) <DreamCommentFilteringService> filteringComment

---
+++ src/main/java/app/jg/og/zamong/service/dream/comment/filtering/DreamCommentFilteringServiceImpl.java
@@ -0,0 +1,43 @@
+package app.jg.og.zamong.service.dream.comment.filtering;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class DreamCommentFilteringServiceImpl implements DreamCommentFilteringService {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3Client amazonS3Client;
+
+    private String[] FILTERING_WORD_LIST;
+
+    @Override
+    public String filteringComment(String content) {
+        int size = FILTERING_WORD_LIST.length;
+
+        for(int i=0; i<size; i++) {
+            String filterWord = FILTERING_WORD_LIST[i];
+
+            if(content.contains(filterWord)) {
+                int s = filterWord.length();
+                content = content.replace(filterWord, "*".repeat(s));
+            }
+        }
+
+        return content;
+    }
+
+    @PostConstruct
+    void postConstruct() throws IOException {
+        String filteringWords = new String(this.amazonS3Client.getObject(bucket, "fword_list.txt").getObjectContent().readAllBytes());
+        FILTERING_WORD_LIST = filteringWords.split("\n");
+    }
+}
