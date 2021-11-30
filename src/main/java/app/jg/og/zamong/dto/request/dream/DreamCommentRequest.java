0b59dfbea50d7141279b38d99005e3dd26078f1c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-30 18:14:53 +0900

    :bug: :: <DreamCommentRequest> 빈문자열 Invalid Parameter 처리

--- src/main/java/app/jg/og/zamong/dto/request/dream/DreamCommentRequest.java
+++ src/main/java/app/jg/og/zamong/dto/request/dream/DreamCommentRequest.java
@@ -3,6 +3,7 @@ package app.jg.og.zamong.dto.request.dream;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -13,6 +14,7 @@ import javax.validation.constraints.Size;
 public class DreamCommentRequest {
 
     @NotNull(message = "내용을 반드시 입력해야 합니다")
+    @NotBlank(message = "내용을 반드시 입력해야 합니다")
     @Size(max = 100, message = "100자 이상 입력할 수 없습니다")
     private String content;
 
